7605391b8e2bdd32e4bfcf88c80ee62a394a880d
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2012-11-06 19:43:12 +0000

    changes for PostListingActivity

---
+++ trunk/venefica-android/Venefica/src/com/venefica/module/listings/post/PostListingActivity.java
@@ -0,0 +1,20 @@
+package com.venefica.module.listings.post;
+
+import com.venefica.activity.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * @author avinash
+ * Activity to post listings/adds
+ */
+public class PostListingActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_post_listing);
+	}
+
+}
