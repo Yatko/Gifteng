62d041a59dace11c35723310ce2a82f82d99dfc4
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2012-11-08 20:38:58 +0000

    Bookmark and MyListing activity added

---
+++ trunk/venefica-android/Venefica/src/com/venefica/module/listings/bookmarks/BookmarkListingsActivity.java
@@ -0,0 +1,117 @@
+package com.venefica.module.listings.bookmarks;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.venefica.activity.R;
+import com.venefica.module.listings.ListingData;
+import com.venefica.module.listings.ListingListAdapter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class BookmarkListingsActivity extends Activity {
+	/**
+	 * List to show bookmark listings
+	 */
+	private ListView listViewListings;
+	/**
+	 * List adapter
+	 */
+	private ListingListAdapter listingsListAdapter;
+	/**
+	 * Listings list
+	 */
+	private ArrayList<ListingData> listings;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmark_listings);
+        listViewListings = (ListView) findViewById(R.id.listActBookmarkListings);
+        listings = getBookmarkListings();
+        
+		listingsListAdapter = new ListingListAdapter(this, listings);
+		listViewListings.setAdapter(listingsListAdapter);
+		listViewListings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+//				Intent intent = new Intent(MyListingsActivity.this, ListingDetailActivity.class);
+//				startActivity(intent);
+			}
+		});
+    }
+
+	private ArrayList<ListingData> getBookmarkListings() {
+		ArrayList<ListingData> listings = new ArrayList<ListingData>();
+		ListingData listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		return listings;
+	}
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_bookmark_listings, menu);
+        return true;
+    }*/
+}
---
+++ trunk/venefica-android/Venefica/src/com/venefica/module/listings/mylistings/MyListingsActivity.java
@@ -0,0 +1,124 @@
+package com.venefica.module.listings.mylistings;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.venefica.activity.R;
+import com.venefica.activity.R.layout;
+import com.venefica.activity.R.menu;
+import com.venefica.module.listings.ListingData;
+import com.venefica.module.listings.ListingListAdapter;
+import com.venefica.module.messages.MessageData;
+import com.venefica.module.messages.MessageDetailActivity;
+import com.venefica.module.messages.MessageListActivity;
+import com.venefica.module.messages.MessageListAdapter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MyListingsActivity extends Activity {
+	/**
+	 * List to show my listings
+	 */
+	private ListView listViewListings;
+	/**
+	 * List adapter
+	 */
+	private ListingListAdapter listingsListAdapter;
+	/**
+	 * Listings list
+	 */
+	private ArrayList<ListingData> listings;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_listings);
+        listViewListings = (ListView) findViewById(R.id.listActMyListings);
+        listings = getDemoListings();
+        
+		listingsListAdapter = new ListingListAdapter(this, listings);
+		listViewListings.setAdapter(listingsListAdapter);
+		listViewListings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+//				Intent intent = new Intent(MyListingsActivity.this, ListingDetailActivity.class);
+//				startActivity(intent);
+			}
+		});
+    }
+
+	private ArrayList<ListingData> getDemoListings() {
+		ArrayList<ListingData> listings = new ArrayList<ListingData>();
+		ListingData listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		listing = new ListingData();
+		listing.setListingId(1);
+		listing.setTitle("Apartment");
+		listing.setDescription("3 bed, kitchen, Living room");
+		listing.setPrice(new BigDecimal(75000.00));
+		listing.setExpiresAt(new Date());
+		listing.setCurrencyCode("USD");
+		listings.add(listing);
+		return listings;
+	}
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_my_listings, menu);
+        return true;
+    }*/
+}
