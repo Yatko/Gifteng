13ffc36e6dccac0f880c0ae5e986fe6c94a03ac5
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2012-11-08 20:34:37 +0000

    removed unused imports

--- trunk/venefica-android/Venefica/src/com/venefica/module/settings/SettingsActivity.java
+++ trunk/venefica-android/Venefica/src/com/venefica/module/settings/SettingsActivity.java
@@ -1,12 +1,9 @@
 package com.venefica.module.settings;
 
 import com.venefica.activity.R;
-import com.venefica.activity.R.layout;
-import com.venefica.activity.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class SettingsActivity extends Activity {
 
