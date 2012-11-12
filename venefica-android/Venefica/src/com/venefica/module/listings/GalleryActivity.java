a2a3b5afd4b1f1554216280519fc4ebc13e5c4f7
avinash.yatko <avinash@venefica.labs>
2012-11-12 18:13:47 +0000

    changes for map view and gallery for listing details

---
+++ venefica-android/Venefica/src/com/venefica/module/listings/GalleryActivity.java
@@ -0,0 +1,101 @@
+package com.venefica.module.listings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.venefica.activity.R;
+import com.venefica.activity.R.layout;
+import com.venefica.activity.R.menu;
+
+import android.os.Bundle;
+import android.provider.MediaStore.Images;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Gallery;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher.ViewFactory;
+
+public class GalleryActivity extends Activity implements ViewFactory {
+	/**
+	 * Gallery to images
+	 */
+	private Gallery gallery;
+
+    private int selectedImagePosition = 0;
+    /**
+     * Images
+     */
+    private List<Drawable> drawables;
+    /**
+     * Adapter for gallery
+     */
+    private GalleryImageAdapter galImageAdapter;
+    /**
+     * Image switcher
+     */
+    private ImageSwitcher imgSwitcher;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+      //Gallery
+        gallery = (Gallery) findViewById(R.id.galleryActGallery);
+        gallery.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int position, long id) {
+				imgSwitcher.setImageDrawable(drawables.get(position));
+				
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+				imgSwitcher.setVisibility(View.GONE);
+			}
+		});
+        drawables = getImages();
+        galImageAdapter = new GalleryImageAdapter(this, drawables);
+        gallery.setAdapter(galImageAdapter);
+        
+        imgSwitcher = (ImageSwitcher) findViewById(R.id.switcherActGallery);
+        imgSwitcher.setFactory(this);
+        imgSwitcher.setInAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in));
+        imgSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out));
+    }
+
+    private List<Drawable> getImages() {
+		drawables = new ArrayList<Drawable>();
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+        drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
+		return drawables;
+	}
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_gallery, menu);
+        return true;
+    }*/
+
+	public View makeView() {
+		ImageView img = new ImageView(this);
+		img.setBackgroundColor(0xFF000000);
+		img.setScaleType(ImageView.ScaleType.FIT_CENTER);
+		img.setLayoutParams(new ImageSwitcher.LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT));
+        return img;
+	}
+}
--- venefica-android/Venefica/src/com/venefica/module/listings/ListingDetailsActivity.java
+++ venefica-android/Venefica/src/com/venefica/module/listings/ListingDetailsActivity.java
@@ -3,13 +3,20 @@ package com.venefica.module.listings;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.android.maps.Overlay;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
 import com.venefica.activity.R;
 import com.venefica.module.utils.Utility;
 
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.Gallery;
 import android.widget.ImageButton;
@@ -19,7 +26,7 @@ import android.widget.TextView;
  * @author avinash
  * Activity to show detail Listing  
  */
-public class ListingDetailsActivity extends Activity {
+public class ListingDetailsActivity extends MapActivity {
 	/**
 	 * Gallery to images
 	 */
@@ -39,9 +46,20 @@ public class ListingDetailsActivity extends Activity {
      */
     private TextView txtTitle, txtPrice, txtDescription, txtAddress, txtListingDate, 
     			txtExpiaryDate;
-    
+    /**
+     * Buttons
+     */
     private Button btnMakeOffer, btnViewSeller;
-    private ImageButton btnBookmark;
+    private ImageButton btnBookmark, btnShowOnMap;
+    
+    /**
+     * Map 
+     */
+    private MapController mapController;
+	private MapView mapView;
+	private LocationManager locationManager;
+	private Overlay itemizedoverlay;
+	private boolean showMap = true;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,8 +71,9 @@ public class ListingDetailsActivity extends Activity {
         txtAddress = (TextView) findViewById(R.id.txtActListingAddress);
         txtListingDate = (TextView) findViewById(R.id.txtActListingListedOn);
 		txtExpiaryDate = (TextView) findViewById(R.id.txtActListingExpiresOn);
-        
+        //Seller
 		btnViewSeller = (Button) findViewById(R.id.btnActListingDetailsSeller);
+		//Bookmark
 		btnBookmark = (ImageButton) findViewById(R.id.btnActListingDetailsBookmark);
 		btnBookmark.setOnClickListener(new View.OnClickListener() {
 			
@@ -62,8 +81,37 @@ public class ListingDetailsActivity extends Activity {
 				Utility.showShortToast(ListingDetailsActivity.this, getResources().getString(R.string.msg_detail_listing_add_fav_success));
 			}
 		});
+		//Map
+		mapView = (MapView) findViewById(R.id.mapviewActListingDetails);
+		mapView.setBuiltInZoomControls(true);
+		// satellite or 2d mode
+		mapView.setSatellite(true);
+		mapController = mapView.getController();
+		mapController.setZoom(14); // Zoom 1 is world view
+		btnShowOnMap = (ImageButton) findViewById(R.id.btnActListingDetailsLocateOnMap);
+		btnShowOnMap.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				if (showMap) {
+					mapView.setVisibility(MapView.VISIBLE);
+					showMap = false;
+				} else {
+					mapView.setVisibility(MapView.GONE);
+					showMap = true;
+				}
+				
+			}
+		});
         //Gallery
         gallery = (Gallery) findViewById(R.id.galleryActListingDetailsPhotos);
+        gallery.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Intent galleryIntent = new Intent(ListingDetailsActivity.this, GalleryActivity.class);
+				startActivity(galleryIntent);
+			}
+		});
         drawables = getImages();
         galImageAdapter = new GalleryImageAdapter(this, drawables);
         gallery.setAdapter(galImageAdapter);
@@ -90,6 +138,11 @@ public class ListingDetailsActivity extends Activity {
         drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
         drawables.add(getResources().getDrawable(R.drawable.ic_launcher));
 		return drawables;
+	}
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
     /*@Override
