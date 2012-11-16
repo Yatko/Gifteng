e5e9fa0da65252ba0f0e88e40c2ca02116eeb78f
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2012-11-16 21:41:38 +0000

    Result wrapper for post listing result

---
+++ trunk/venefica-android/Venefica/src/com/venefica/module/listings/post/PostListingResultWrapper.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.venefica.module.listings.post;
+
+/**
+ * @author avinash
+ *  Wrapper class for post listing result data
+ */
+public class PostListingResultWrapper {
+	public int result = -1;
+	public String data = null;
+}
