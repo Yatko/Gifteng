5e04df2650fa707252d5b60f9fed46d68eba89c4
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2012-11-19 19:36:36 +0000

    wrapper to hold result

---
+++ trunk/venefica-android/Venefica/src/com/venefica/module/listings/browse/BrowseCatResultWrapper.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.venefica.module.listings.browse;
+
+import java.util.List;
+
+import com.venefica.services.CategoryDto;
+
+/**
+ * @author avinash
+ *
+ */
+public class BrowseCatResultWrapper {
+	public int result = -1;
+	public String data = null;
+	public List<CategoryDto> categories;
+}
