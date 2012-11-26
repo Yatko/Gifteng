b45be4a28cb640cb4d692beb54d182edf72713c4
avinash.yatko <avinash@venefica.labs>
2012-11-26 14:54:18 +0000

    wrapper class for search listing result

---
+++ venefica-android/Venefica/src/com/venefica/module/listings/browse/SearchListingResultWrapper.java
@@ -0,0 +1,15 @@
+package com.venefica.module.listings.browse;
+
+import java.util.List;
+
+import com.venefica.services.AdDto;
+
+/**
+ * @author avinash
+ * Search listing result wrapperS
+ */
+public class SearchListingResultWrapper {
+	public int result = -1;
+	public String data = null;
+	public List<AdDto> listings;
+}
