58be8933b256d1904c74409f4ad85ea839f938b9
avinash.yatko <avinash@venefica.labs>
2012-12-14 17:18:22 +0000

    Implemented settings in standard android fromat

--- venefica-android/Venefica/src/com/venefica/module/settings/SettingsActivity.java
+++ venefica-android/Venefica/src/com/venefica/module/settings/SettingsActivity.java
@@ -1,16 +1,30 @@
 package com.venefica.module.settings;
 
-import com.venefica.module.main.R;
-
 import android.os.Bundle;
-import android.app.Activity;
 
-public class SettingsActivity extends Activity {
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.venefica.module.main.R;
+import com.venefica.utils.Constants;
+
+public class SettingsActivity extends SherlockPreferenceActivity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	setTheme(com.actionbarsherlock.R.style.Theme_Sherlock_Light_DarkActionBar);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getPreferenceManager().setSharedPreferencesName(Constants.VENEFICA_PREFERENCES);
+        addPreferencesFromResource(R.xml.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	if (item.getItemId() == android.R.id.home) {
+			finish();
+		}
+		return true;
+    }
+    
+    
 }
