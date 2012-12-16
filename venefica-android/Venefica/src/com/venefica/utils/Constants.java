b7f8e2430c13a68b9d753214024c8de7f7b319bf
avinash.yatko <avinash@venefica.labs>
2012-12-16 20:59:06 +0000

    constants added for image size limit

--- venefica-android/Venefica/src/com/venefica/utils/Constants.java
+++ venefica-android/Venefica/src/com/venefica/utils/Constants.java
@@ -62,7 +62,8 @@ public class Constants
 	public static final String CAMERA_FOLDER = "CameraPicture";
 	public static final String CACHE_FOLDER = "Cache";
 	public static final boolean USE_PNG_COMPRESS = false;
-	public static final int IMAGE_MAX_SIZE = 320;
+	public static final int IMAGE_MAX_SIZE_X= 320;
+	public static final int IMAGE_MAX_SIZE_Y= 360;
 	public static final int IMAGE_THUMBNAILS_MAX_SIZE = 64;
 	public static final long GEOLOCATION_UPDATE_TIME_MS = 30 * 60 * 1000;
 	
