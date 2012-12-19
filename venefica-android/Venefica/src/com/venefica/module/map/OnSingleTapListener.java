0e5a2fe7c24ccae2242c051ef9d25f90cd2759ae
avinash.yatko <avinash@venefica.labs>
2012-12-19 20:49:20 +0000

    added to show popup on tap of map overlay item

---
+++ venefica-android/Venefica/src/com/venefica/module/map/OnSingleTapListener.java
@@ -0,0 +1,11 @@
+package com.venefica.module.map;
+
+import android.view.MotionEvent;
+
+/**
+ * @author avinash
+ * Listener for TapContolledMapView
+ */
+public interface OnSingleTapListener {
+	public boolean onSingleTap(MotionEvent e);
+}
