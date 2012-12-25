79bfe626641058da73a5031e4f39ec15b9e35d91
avinash.yatko <avinash@venefica.labs>
2012-12-25 19:51:25 +0000

    modifications for filter settings

--- venefica-android/Venefica/src/com/venefica/module/settings/SettingsActivity.java
+++ venefica-android/Venefica/src/com/venefica/module/settings/SettingsActivity.java
@@ -1,6 +1,11 @@
 package com.venefica.module.settings;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.MenuItem;
@@ -9,13 +14,23 @@ import com.venefica.utils.Constants;
 
 public class SettingsActivity extends SherlockPreferenceActivity{
 
+	public static final int ACT_MODE_FILTER_SETTINGS = 1001;
+	public static final int ACT_MODE_APP_SETTINGS = 1002;
+	public static int CURRENT_MODE = ACT_MODE_FILTER_SETTINGS;
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	setTheme(com.actionbarsherlock.R.style.Theme_Sherlock_Light_DarkActionBar);
         super.onCreate(savedInstanceState);
-        getPreferenceManager().setSharedPreferencesName(Constants.VENEFICA_PREFERENCES);
-        addPreferencesFromResource(R.xml.activity_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getPreferenceManager().setSharedPreferencesName(Constants.VENEFICA_PREFERENCES);
+        getPreferenceManager().setSharedPreferencesMode(Activity.MODE_PRIVATE);
+        
+        CURRENT_MODE = getIntent().getIntExtra("act_mode", ACT_MODE_FILTER_SETTINGS);
+        if( CURRENT_MODE == ACT_MODE_FILTER_SETTINGS){
+        	addPreferencesFromResource(R.xml.activity_search_filter_settings);
+        } else {
+        	addPreferencesFromResource(R.xml.activity_settings);
+        }
     }
 
     @Override
@@ -26,5 +41,9 @@ public class SettingsActivity extends SherlockPreferenceActivity{
 		return true;
     }
     
-    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	Log.d("Settings", data.getStringExtra("category_name"));
+    	super.onActivityResult(requestCode, resultCode, data);
+    }
 }
