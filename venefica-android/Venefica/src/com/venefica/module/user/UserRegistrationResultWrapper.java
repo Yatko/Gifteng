23386cdb172d9a89744bda55cfa5424f465da874
avinash.yatko <avinash@venefica.labs>
2013-01-29 18:02:30 +0000

    changes to use UserDto

--- venefica-android/Venefica/src/com/venefica/module/user/UserRegistrationResultWrapper.java
+++ venefica-android/Venefica/src/com/venefica/module/user/UserRegistrationResultWrapper.java
@@ -3,14 +3,12 @@
  */
 package com.venefica.module.user;
 
-import com.venefica.services.User;
-
 /**
  * @author avinash
  * Wrapper class for user registration result data 
  */
 public class UserRegistrationResultWrapper {
 	public int result = -1;
-	public User userData = null;
+	public UserDto userDto = null;
 	public String data = null;
 }
