b184e900788e7d0d7529e51c3211f15a03c4dde1
avinash.yatko <avinash@venefica.labs>
2013-02-18 13:16:28 +0000

    class to handle camera preview

---
+++ venefica-android/Venefica/src/com/venefica/module/utils/CameraPreview.java
@@ -0,0 +1,87 @@
+package com.venefica.module.utils;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/** A basic Camera preview class */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context) {
+        super(context);
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+    public void setCamera(Camera camera){
+    	mCamera = camera;
+    }
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d("CameraPreview :: ", "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    	
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+          // preview surface does not exist
+          return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+          // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPictureFormat(PixelFormat.JPEG); 
+        parameters.set("orientation", "portrait");
+        parameters.setRotation(90);
+        setDisplayOrientation(mCamera, 90);
+        mCamera.setParameters(parameters);
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d("CameraPreview :: ", "Error starting camera preview: " + e.getMessage());
+        }
+    }
+    
+	protected void setDisplayOrientation(Camera camera, int angle) {
+		Method downPolymorphic;
+		try {
+			downPolymorphic = camera.getClass().getMethod(
+					"setDisplayOrientation", new Class[] { int.class });
+			if (downPolymorphic != null)
+				downPolymorphic.invoke(camera, new Object[] { angle });
+		} catch (Exception e1) {
+		}
+	}
+}
