d4b88a8dcf15cead137866d655489c80ee1cdffd
Yatko <yatko@users.noreply.github.com>
2013-02-20 18:56:24 +0000

    reformat

--- venefica-webapp/src/main/java/com/venefica/dao/RatingDaoImpl.java
+++ venefica-webapp/src/main/java/com/venefica/dao/RatingDaoImpl.java
@@ -1,30 +1,27 @@
 package com.venefica.dao;
 
+import com.venefica.model.Rating;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.venefica.model.Rating;
-
-
 /**
  * Implementation of {@link RatingDao} interface.
- * 
+ *
  * @author Sviatoslav Grebenchukov
- * 
+ *
  */
 @Repository
 @Transactional(propagation = Propagation.REQUIRED)
 public class RatingDaoImpl extends DaoBase implements RatingDao {
 
-	@Override
-	public Long save(Rating rating) {
-		return saveEntity(rating);
-	}
+    @Override
+    public Long save(Rating rating) {
+        return saveEntity(rating);
+    }
 
-	@Override
-	public void delete(Rating rating) {
-		deleteEntity(rating);
-	}
-
+    @Override
+    public void delete(Rating rating) {
+        deleteEntity(rating);
+    }
 }
--- venefica-webapp/src/main/java/com/venefica/dao/SpamMarkDao.java
+++ venefica-webapp/src/main/java/com/venefica/dao/SpamMarkDao.java
@@ -4,25 +4,23 @@ import com.venefica.model.SpamMark;
 
 /**
  * Data access interface for {@link SpamMark} entity.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 public interface SpamMarkDao {
 
-	/**
-	 * Saves the spam mark in the database.
-	 * 
-	 * @param mark
-	 *            mark to save
-	 * @return id of the stored mark
-	 */
-	Long save(SpamMark mark);
+    /**
+     * Saves the spam mark in the database.
+     *
+     * @param mark mark to save
+     * @return id of the stored mark
+     */
+    Long save(SpamMark mark);
 
-	/**
-	 * Removes the spam mark from the database.
-	 * 
-	 * @param mark
-	 *            mark to remvoe
-	 */
-	void delete(SpamMark mark);
+    /**
+     * Removes the spam mark from the database.
+     *
+     * @param mark mark to remvoe
+     */
+    void delete(SpamMark mark);
 }
