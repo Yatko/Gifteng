2aab575e1e2163ce2c4f4c166d747e1f5ffc8d03
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-20 19:14:13 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/service/dto/ImageDto.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/service/dto/ImageDto.java
@@ -1,91 +1,90 @@
 package com.venefica.service.dto;
 
+import com.venefica.model.Image;
+import com.venefica.model.ImageType;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
-import com.venefica.model.Image;
-import com.venefica.model.ImageType;
-
-
 /**
  * Image data transfer object.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ImageDto extends DtoBase {
-	public static final String BASE_PATH = "/images/";
-	public static final String NOIMAGE = "noimage";
 
-	// out
-	private Long id;
-	// in
-	private ImageType imgType;
-	// in
-	private byte[] data;
-	// out
-	private String url;
+    public static final String BASE_PATH = "/images/";
+    public static final String NOIMAGE = "noimage";
+    
+    // out
+    private Long id;
+    // in
+    private ImageType imgType;
+    // in
+    private byte[] data;
+    // out
+    private String url;
 
-	// required for JAX-WS
-	public ImageDto() {
-	}
-	
-	public static String imageUrl(Image image) {
-		return image != null ? BASE_PATH + "img" + image.getId().toString() : null;
-	}
+    // required for JAX-WS
+    public ImageDto() {
+    }
 
-	public ImageDto(Image image) {
-		this.id = image.getId();
-		this.url = imageUrl(image);
-	}
-	
-	public Long getId() {
-		return id;
-	}
+    public static String imageUrl(Image image) {
+        return image != null ? BASE_PATH + "img" + image.getId().toString() : null;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public ImageDto(Image image) {
+        this.id = image.getId();
+        this.url = imageUrl(image);
+    }
 
-	public boolean isValid() {
-		return imgType != null && data != null;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Image toImage() {
-		return new Image(imgType, data);
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void update(Image image) {
-		image.setImgType(imgType);
-		image.setData(data);
-	}
+    public boolean isValid() {
+        return imgType != null && data != null;
+    }
 
-	public ImageDto(ImageType imgType, byte[] data) {
-		this.imgType = imgType;
-		this.data = data;
-	}
+    public Image toImage() {
+        return new Image(imgType, data);
+    }
 
-	public ImageType getImgType() {
-		return imgType;
-	}
+    public void update(Image image) {
+        image.setImgType(imgType);
+        image.setData(data);
+    }
 
-	public void setImgType(ImageType imgType) {
-		this.imgType = imgType;
-	}
+    public ImageDto(ImageType imgType, byte[] data) {
+        this.imgType = imgType;
+        this.data = data;
+    }
 
-	public byte[] getData() {
-		return data;
-	}
+    public ImageType getImgType() {
+        return imgType;
+    }
 
-	public void setData(byte[] data) {
-		this.data = data;
-	}
+    public void setImgType(ImageType imgType) {
+        this.imgType = imgType;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public byte[] getData() {
+        return data;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
