c3e34f4ad37cad943a963333f55462e78e35c55e
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-20 18:57:00 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/dao/UserDaoImpl.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/dao/UserDaoImpl.java
@@ -1,86 +1,86 @@
 package com.venefica.dao;
 
+import com.venefica.model.User;
 import java.util.List;
-
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.venefica.model.User;
-
 /**
- * Implementation of @{link {@link UserDao}.
- * 
+ * Implementation of
+ *
+ * @{link {@link UserDao}.
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Repository
 @Transactional(propagation = Propagation.REQUIRED)
 public class UserDaoImpl extends DaoBase implements UserDao {
 
-	@Override
-	public User get(Long id) {
-		return (User) getCurrentSession().get(User.class, id);
-	}
+    @Override
+    public User get(Long id) {
+        return (User) getCurrentSession().get(User.class, id);
+    }
 
-	public User findUserByName(String name) {
-		@SuppressWarnings("rawtypes")
-		List users = getCurrentSession().createQuery("from User where name=:name")
-				.setParameter("name", name).list();
+    @Override
+    public User findUserByName(String name) {
+        @SuppressWarnings("rawtypes")
+        List users = getCurrentSession().createQuery("from User where name=:name")
+                .setParameter("name", name).list();
 
-		return users.size() == 0 ? null : (User) users.get(0);
-	}
+        return users.isEmpty() ? null : (User) users.get(0);
+    }
 
-	@Override
-	public User findUserByEmail(String email) {
-		@SuppressWarnings("rawtypes")
-		List users = getCurrentSession().createQuery("from User where email=:email")
-				.setParameter("email", email).list();
+    @Override
+    public User findUserByEmail(String email) {
+        @SuppressWarnings("rawtypes")
+        List users = getCurrentSession().createQuery("from User where email=:email")
+                .setParameter("email", email).list();
 
-		return users.size() == 0 ? null : (User) users.get(0);
-	}
+        return users.isEmpty() ? null : (User) users.get(0);
+    }
 
-	@Override
-	public User findUserByPhoneNumber(String phoneNumber) {
-		@SuppressWarnings("rawtypes")
-		List users = getCurrentSession().createQuery("from User where phoneNumber=:phoneNumber")
-				.setParameter("phoneNumber", phoneNumber).list();
+    @Override
+    public User findUserByPhoneNumber(String phoneNumber) {
+        @SuppressWarnings("rawtypes")
+        List users = getCurrentSession().createQuery("from User where phoneNumber=:phoneNumber")
+                .setParameter("phoneNumber", phoneNumber).list();
 
-		return users.size() == 0 ? null : (User) users.get(0);
-	}
+        return users.isEmpty() ? null : (User) users.get(0);
+    }
 
-	public Long save(User user) {
-		return (Long) getCurrentSession().save(user);
-	}
+    @Override
+    public Long save(User user) {
+        return (Long) getCurrentSession().save(user);
+    }
 
-	@Override
-	public void update(User user) {
-		getCurrentSession().update(user);
-	}
+    @Override
+    public void update(User user) {
+        getCurrentSession().update(user);
+    }
 
-	@Override
-	public boolean removeByName(String name) {
-		int numUserDeleted = getCurrentSession()
-				.createQuery("delete from User u where u.name = :name").setParameter("name", name)
-				.executeUpdate();
+    @Override
+    public boolean removeByName(String name) {
+        int numUserDeleted = getCurrentSession()
+                .createQuery("delete from User u where u.name = :name").setParameter("name", name)
+                .executeUpdate();
 
-		return numUserDeleted != 0;
-	}
+        return numUserDeleted != 0;
+    }
 
-	@Override
-	public boolean removeByEmail(String email) {
-		int numUserDeleted = getCurrentSession()
-				.createQuery("delete from User u where u.email = :email")
-				.setParameter("email", email).executeUpdate();
+    @Override
+    public boolean removeByEmail(String email) {
+        int numUserDeleted = getCurrentSession()
+                .createQuery("delete from User u where u.email = :email")
+                .setParameter("email", email).executeUpdate();
 
-		return numUserDeleted != 0;
-	}
+        return numUserDeleted != 0;
+    }
 
-	@Override
-	public Long getMaxUserId() {
-		Long maxId = (Long) getCurrentSession().createQuery("select max(u.id) from User u")
-				.uniqueResult();
-		return maxId != null ? maxId : 0;
-	}
-
+    @Override
+    public Long getMaxUserId() {
+        Long maxId = (Long) getCurrentSession().createQuery("select max(u.id) from User u")
+                .uniqueResult();
+        return maxId != null ? maxId : 0;
+    }
 }
