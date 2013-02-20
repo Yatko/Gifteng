fe94bba9df528c7b2f63127e18dd7a20f9eae946
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-20 18:54:44 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/dao/CommentDao.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/dao/CommentDao.java
@@ -4,26 +4,24 @@ import com.venefica.model.Comment;
 
 /**
  * Data access interface for {@link Comment} entity.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 public interface CommentDao {
 
-	/**
-	 * Saves the comment in the database
-	 * 
-	 * @param comment
-	 *            comment to save
-	 * @return id of the saved comment
-	 */
-	Long save(Comment message);
+    /**
+     * Saves the comment in the database
+     *
+     * @param comment comment to save
+     * @return id of the saved comment
+     */
+    Long save(Comment message);
 
-	/**
-	 * Returns the comment by its id.
-	 * 
-	 * @param id
-	 *            id of the comment
-	 * @return comment or null if a comment with the specified id not found
-	 */
-	Comment get(Long id);
+    /**
+     * Returns the comment by its id.
+     *
+     * @param id id of the comment
+     * @return comment or null if a comment with the specified id not found
+     */
+    Comment get(Long id);
 }
