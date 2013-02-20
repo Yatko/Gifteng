ed53b01f50647de91dadc602d3781aaaab2e6fc2
Yatko <yatko@users.noreply.github.com>
2013-02-20 18:55:39 +0000

    reformat

--- venefica-webapp/src/main/java/com/venefica/dao/MessageDaoImpl.java
+++ venefica-webapp/src/main/java/com/venefica/dao/MessageDaoImpl.java
@@ -1,33 +1,31 @@
 package com.venefica.dao;
 
-
+import com.venefica.model.Message;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.venefica.model.Message;
-
 /**
  * Implementation of {@link MessageDao} interface.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Repository
 @Transactional(propagation = Propagation.REQUIRED)
 public class MessageDaoImpl extends DaoBase implements MessageDao {
 
-	@Override
-	public Long save(Message message) {
-		return saveEntity(message);
-	}
+    @Override
+    public Long save(Message message) {
+        return saveEntity(message);
+    }
 
-	@Override
-	public Message get(Long id) {
-		return (Message) getEntity(Message.class, id);
-	}
+    @Override
+    public Message get(Long id) {
+        return (Message) getEntity(Message.class, id);
+    }
 
-	@Override
-	public void deleteMessage(Message message) {
-		deleteEntity(message);
-	}	
+    @Override
+    public void deleteMessage(Message message) {
+        deleteEntity(message);
+    }
 }
