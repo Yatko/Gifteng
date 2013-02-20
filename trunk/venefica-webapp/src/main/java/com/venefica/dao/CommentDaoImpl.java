ad512dd0b8de3e75ec212d76009926a2d093dd7e
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-20 18:58:17 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/dao/CommentDaoImpl.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/dao/CommentDaoImpl.java
@@ -1,28 +1,26 @@
 package com.venefica.dao;
 
-
+import com.venefica.model.Comment;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.venefica.model.Comment;
-
 /**
  * Implementation of {@link CommentDao} interface.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Repository
 @Transactional(propagation = Propagation.REQUIRED)
 public class CommentDaoImpl extends DaoBase implements CommentDao {
 
-	@Override
-	public Long save(Comment comment) {
-		return saveEntity(comment);
-	}
+    @Override
+    public Long save(Comment comment) {
+        return saveEntity(comment);
+    }
 
-	@Override
-	public Comment get(Long id) {
-		return (Comment) getEntity(Comment.class, id);
-	}
+    @Override
+    public Comment get(Long id) {
+        return (Comment) getEntity(Comment.class, id);
+    }
 }
