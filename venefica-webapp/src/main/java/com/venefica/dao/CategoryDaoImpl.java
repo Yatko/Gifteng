53f131327178a19f7b5e36a3f8b04d80c2d59088
Yatko <yatko@users.noreply.github.com>
2013-02-20 18:58:17 +0000

    reformat

--- venefica-webapp/src/main/java/com/venefica/dao/CategoryDaoImpl.java
+++ venefica-webapp/src/main/java/com/venefica/dao/CategoryDaoImpl.java
@@ -1,66 +1,64 @@
 package com.venefica.dao;
 
+import com.venefica.model.Category;
 import java.util.List;
-
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.venefica.model.Category;
-
 /**
  * Implementation of {@link CategoryDao} interface.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Repository
 @Transactional(propagation = Propagation.REQUIRED)
 public class CategoryDaoImpl extends DaoBase implements CategoryDao {
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public List<Category> getSubcategories(Long parentCategoryId) {
-		List<Category> categories;
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Category> getSubcategories(Long parentCategoryId) {
+        List<Category> categories;
 
-		if (parentCategoryId == null)
-			categories = (List<Category>) getCurrentSession().createQuery(
-					"from Category c where c.parent is null").list();
-		else
-			categories = (List<Category>) getCurrentSession()
-					.createQuery("from Category c where c.parent.id = :parentId")
-					.setParameter("parentId", parentCategoryId).list();
+        if (parentCategoryId == null) {
+            categories = (List<Category>) getCurrentSession().createQuery(
+                    "from Category c where c.parent is null").list();
+        } else {
+            categories = (List<Category>) getCurrentSession()
+                    .createQuery("from Category c where c.parent.id = :parentId")
+                    .setParameter("parentId", parentCategoryId).list();
+        }
 
-		return categories;
-	}
+        return categories;
+    }
 
-	@Override
-	public Category findByName(String name) {
-		List<?> categories = getCurrentSession()
-				.createQuery("from Category c where c.name = :name").setParameter("name", name)
-				.list();
+    @Override
+    public Category findByName(String name) {
+        List<?> categories = getCurrentSession()
+                .createQuery("from Category c where c.name = :name").setParameter("name", name)
+                .list();
 
-		return categories.size() == 0 ? null : (Category) categories.get(0);
-	}
+        return categories.isEmpty() ? null : (Category) categories.get(0);
+    }
 
-	@Override
-	public Long save(Category category) {
-		return (Long) getCurrentSession().save(category);
-	}
+    @Override
+    public Long save(Category category) {
+        return (Long) getCurrentSession().save(category);
+    }
 
-	@Override
-	public boolean hasCategories() {
-		Long numCategories = (Long) getCurrentSession()
-				.createQuery("select count(*) from Category").uniqueResult();
+    @Override
+    public boolean hasCategories() {
+        Long numCategories = (Long) getCurrentSession()
+                .createQuery("select count(*) from Category").uniqueResult();
 
-		return numCategories > 0;
-	}
+        return numCategories > 0;
+    }
 
-	@Override
-	public Category get(Long categoryId) {
-		if (categoryId == null)
-			throw new NullPointerException("categoriId");
-		return (Category) getCurrentSession().get(Category.class, categoryId);
-	}
-
+    @Override
+    public Category get(Long categoryId) {
+        if (categoryId == null) {
+            throw new NullPointerException("categoriId");
+        }
+        return (Category) getCurrentSession().get(Category.class, categoryId);
+    }
 }
--- venefica-webapp/src/main/java/com/venefica/dao/ImageDaoImpl.java
+++ venefica-webapp/src/main/java/com/venefica/dao/ImageDaoImpl.java
@@ -1,33 +1,31 @@
 package com.venefica.dao;
 
+import com.venefica.model.Image;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.venefica.model.Image;
-
-
 /**
  * Implementation of {@link ImageDao} interface.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Repository
 @Transactional(propagation = Propagation.REQUIRED)
 public class ImageDaoImpl extends DaoBase implements ImageDao {
 
-	@Override
-	public Long save(Image image) {
-		return saveEntity(image);
-	}
+    @Override
+    public Long save(Image image) {
+        return saveEntity(image);
+    }
 
-	@Override
-	public Image get(Long imageId) {
-		return (Image) getEntity(Image.class, imageId);
-	}
+    @Override
+    public Image get(Long imageId) {
+        return (Image) getEntity(Image.class, imageId);
+    }
 
-	@Override
-	public void delete(Image image) {
-		deleteEntity(image);
-	}
+    @Override
+    public void delete(Image image) {
+        deleteEntity(image);
+    }
 }
