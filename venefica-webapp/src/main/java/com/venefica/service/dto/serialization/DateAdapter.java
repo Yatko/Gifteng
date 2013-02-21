37195a5355cd9c361cbd027e2a4d10b5f980e18d
Yatko <yatko@users.noreply.github.com>
2013-02-21 06:31:45 +0000

    added CalendarAdapter to be used whenever possible instead of Date

--- venefica-webapp/src/main/java/com/venefica/service/dto/serialization/DateAdapter.java
+++ venefica-webapp/src/main/java/com/venefica/service/dto/serialization/DateAdapter.java
@@ -7,7 +7,10 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
  * Used to serialize (deserialize) a date object within a soap message.
  *
  * @author Sviatoslav Grebenchukov
+ * @deprecated Should be avoided to be used as the Date class is deprecated in java.
+ * Use instead Calendar instead whenever possible.
  */
+@Deprecated
 public class DateAdapter extends XmlAdapter<String, Date> {
 
     @Override
