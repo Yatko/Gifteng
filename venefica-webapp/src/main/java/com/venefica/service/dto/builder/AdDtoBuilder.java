36578ddc61631effacec3c918c055ba6bf68f2e0
Yatko <yatko@users.noreply.github.com>
2013-02-21 06:16:07 +0000

    reformat

--- venefica-webapp/src/main/java/com/venefica/service/dto/builder/AdDtoBuilder.java
+++ venefica-webapp/src/main/java/com/venefica/service/dto/builder/AdDtoBuilder.java
@@ -1,7 +1,5 @@
 package com.venefica.service.dto.builder;
 
-import java.util.LinkedList;
-
 import com.venefica.model.Ad;
 import com.venefica.model.Image;
 import com.venefica.model.Rating;
@@ -10,121 +8,124 @@ import com.venefica.model.User;
 import com.venefica.service.dto.AdDto;
 import com.venefica.service.dto.ImageDto;
 import com.venefica.service.dto.UserDto;
-
+import java.util.LinkedList;
 
 /**
  * Simplifies DTO object constructing.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 public class AdDtoBuilder extends DtoBuilderBase<Ad, AdDto> {
-	private User currentUser;
-	private boolean includeCreatorFlag;
-	private boolean includeImagesFlag;
-	private boolean includeCanMarkAsSpamFlag;
-	private boolean includeCanRateFlag;
 
-	public AdDtoBuilder(Ad model) {
-		super(model);
-	}
-	
-	public AdDtoBuilder setCurrentUser(User user)  {
-		currentUser = user;
-		return this;
-	}
+    private User currentUser;
+    private boolean includeCreatorFlag;
+    private boolean includeImagesFlag;
+    private boolean includeCanMarkAsSpamFlag;
+    private boolean includeCanRateFlag;
 
-	public AdDtoBuilder includeCreator() {
-		includeCreatorFlag = true;
-		return this;
-	}
+    public AdDtoBuilder(Ad model) {
+        super(model);
+    }
 
-	public AdDtoBuilder includeImages() {
-		includeImagesFlag = true;
-		return this;
-	}
+    public AdDtoBuilder setCurrentUser(User user) {
+        currentUser = user;
+        return this;
+    }
 
-	public AdDtoBuilder includeCanMarkAsSpam() {
-		includeCanMarkAsSpamFlag = true;
-		return this;
-	}
-	
-	public AdDtoBuilder includeCanRate() {
-		includeCanRateFlag = true;
-		return this;
-	}
+    public AdDtoBuilder includeCreator() {
+        includeCreatorFlag = true;
+        return this;
+    }
 
-	@Override
-	public AdDto build() {
-		AdDto adDto = new AdDto();
-		adDto.setId(model.getId());
-		adDto.setCategoryId(model.getCategory().getId());
-		adDto.setCategory(model.getCategory().getName());
-		adDto.setTitle(model.getTitle());
-		adDto.setDescription(model.getDescription());
-		adDto.setPrice(model.getPrice());
+    public AdDtoBuilder includeImages() {
+        includeImagesFlag = true;
+        return this;
+    }
 
-		if (model.getLocation() != null) {
-			adDto.setLatitude(model.getLocation().getY());
-			adDto.setLongitude(model.getLocation().getX());
-		}
+    public AdDtoBuilder includeCanMarkAsSpam() {
+        includeCanMarkAsSpamFlag = true;
+        return this;
+    }
 
-		if (model.getMainImage() != null)
-			adDto.setImage(new ImageDto(model.getMainImage()));
+    public AdDtoBuilder includeCanRate() {
+        includeCanRateFlag = true;
+        return this;
+    }
 
-		if (model.getThumbImage() != null)
-			adDto.setImageThumbnail(new ImageDto(model.getThumbImage()));
+    @Override
+    public AdDto build() {
+        AdDto adDto = new AdDto();
+        adDto.setId(model.getId());
+        adDto.setCategoryId(model.getCategory().getId());
+        adDto.setCategory(model.getCategory().getName());
+        adDto.setTitle(model.getTitle());
+        adDto.setDescription(model.getDescription());
+        adDto.setPrice(model.getPrice());
 
-		if (includeImagesFlag) {
-			LinkedList<ImageDto> images = new LinkedList<ImageDto>();
+        if (model.getLocation() != null) {
+            adDto.setLatitude(model.getLocation().getY());
+            adDto.setLongitude(model.getLocation().getX());
+        }
 
-			for (Image image : model.getImages()) {
-				ImageDto imageDto = new ImageDto(image);
-				images.add(imageDto);
-			}
+        if (model.getMainImage() != null) {
+            adDto.setImage(new ImageDto(model.getMainImage()));
+        }
 
-			adDto.setImages(images);
-		}
+        if (model.getThumbImage() != null) {
+            adDto.setImageThumbnail(new ImageDto(model.getThumbImage()));
+        }
 
-		adDto.setCreatedAt(model.getCreatedAt());
-		adDto.setWanted(model.isWanted());
-		adDto.setExpired(model.isExpired());
-		adDto.setExpiresAt(model.getExpiresAt());
-		adDto.setNumAvailProlongations(model.getNumAvailProlongations());
-		adDto.setNumViews(model.getNumViews());
-		adDto.setRating(model.getRating());
+        if (includeImagesFlag) {
+            LinkedList<ImageDto> images = new LinkedList<ImageDto>();
 
-		adDto.setOwner(currentUser != null && model.getCreator().equals(currentUser));
+            for (Image image : model.getImages()) {
+                ImageDto imageDto = new ImageDto(image);
+                images.add(imageDto);
+            }
 
-		if (includeCreatorFlag) {
-			adDto.setCreator(new UserDto(model.getCreator()));
-		}
+            adDto.setImages(images);
+        }
 
-		if (includeCanMarkAsSpamFlag) {
-			boolean canMarkAsSpam = true;
+        adDto.setCreatedAt(model.getCreatedAt());
+        adDto.setWanted(model.isWanted());
+        adDto.setExpired(model.isExpired());
+        adDto.setExpiresAt(model.getExpiresAt());
+        adDto.setNumAvailProlongations(model.getNumAvailProlongations());
+        adDto.setNumViews(model.getNumViews());
+        adDto.setRating(model.getRating());
 
-			for (SpamMark spamMark : model.getSpamMarks()) {
-				if (spamMark.getWitness().equals(currentUser)) {
-					canMarkAsSpam = false;
-					break;
-				}
-			}
+        adDto.setOwner(currentUser != null && model.getCreator().equals(currentUser));
 
-			adDto.setCanMarkAsSpam(canMarkAsSpam);
-		}
-		
-		if (includeCanRateFlag) {
-			boolean canRate = true;
-			
-			for (Rating rating : model.getRatings()) {
-				if (rating.getUser().equals(currentUser)) {
-					canRate = false;
-					break;
-				}
-			}
-			
-			adDto.setCanRate(canRate);
-		}
+        if (includeCreatorFlag) {
+            adDto.setCreator(new UserDto(model.getCreator()));
+        }
 
-		return adDto;
-	}
+        if (includeCanMarkAsSpamFlag) {
+            boolean canMarkAsSpam = true;
+
+            for (SpamMark spamMark : model.getSpamMarks()) {
+                if (spamMark.getWitness().equals(currentUser)) {
+                    canMarkAsSpam = false;
+                    break;
+                }
+            }
+
+            adDto.setCanMarkAsSpam(canMarkAsSpam);
+        }
+
+        if (includeCanRateFlag) {
+            boolean canRate = true;
+
+            for (Rating rating : model.getRatings()) {
+                if (rating.getUser().equals(currentUser)) {
+                    canRate = false;
+                    break;
+                }
+            }
+
+            adDto.setCanRate(canRate);
+        }
+
+        return adDto;
+    }
 }
--- venefica-webapp/src/main/java/com/venefica/service/dto/builder/DtoBuilderBase.java
+++ venefica-webapp/src/main/java/com/venefica/service/dto/builder/DtoBuilderBase.java
@@ -4,23 +4,23 @@ import com.venefica.service.dto.DtoBase;
 
 /**
  * Base DTO object builder.
- * 
+ *
  * @author Sviatoslav Grebenchukov
- * 
- * @param <M>
- *            model class
- * @param <D>
- *            dto class
+ *
+ * @param <M> model class
+ * @param <D> dto class
  */
 public abstract class DtoBuilderBase<M, D extends DtoBase> {
-	protected M model;	
 
-	public DtoBuilderBase(M model) {
-		if (model == null)
-			throw new NullPointerException("model");
+    protected M model;
 
-		this.model = model;
-	}
-	
-	public abstract D build();
+    public DtoBuilderBase(M model) {
+        if (model == null) {
+            throw new NullPointerException("model");
+        }
+
+        this.model = model;
+    }
+
+    public abstract D build();
 }
