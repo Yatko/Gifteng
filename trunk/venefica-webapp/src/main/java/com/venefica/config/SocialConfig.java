9c1773f6b17172497309c4887f74c3b0000cc370
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-21 07:00:01 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/config/SocialConfig.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/config/SocialConfig.java
@@ -1,9 +1,11 @@
 package com.venefica.config;
 
+import com.venefica.auth.ThreadSecurityContextHolder;
+import com.venefica.connect.ConnectSupport;
+import com.venefica.connect.UserSignInAdapter;
+import com.venefica.connect.UserSignUpAdapter;
 import javax.inject.Inject;
 import javax.sql.DataSource;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -20,87 +22,84 @@ import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.twitter.connect.TwitterConnectionFactory;
 import org.springframework.social.vkontakte.connect.VKontakteConnectionFactory;
 
-import com.venefica.auth.ThreadSecurityContextHolder;
-import com.venefica.connect.ConnectSupport;
-import com.venefica.connect.UserSignInAdapter;
-import com.venefica.connect.UserSignUpAdapter;
-
 /**
  * Spring social configuration.
- * 
+ *
  * @author Sviatoslav Grebenchukov
- * 
+ *
  */
 @Configuration
 public class SocialConfig {
 
-	@Inject
-	private DataSource dataSource;
+    @Inject
+    private DataSource dataSource;
+    
+    @Inject
+    private Environment environment;
+    
+    @Inject
+    private ThreadSecurityContextHolder securityContextHolder;
 
-	@Inject
-	private Environment environment;
+    /**
+     * When a new provider is added to the app register its
+     * {@link ConnectionFactory} here.
+     */
+    @Bean
+    public ConnectionFactoryLocator connectionFactoryLocator() {
+        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+        registry.addConnectionFactory(new FacebookConnectionFactory(environment
+                .getProperty("facebook.clientId"), environment.getProperty("facebook.clientSecret")));
+        registry.addConnectionFactory(new TwitterConnectionFactory(environment
+                .getProperty("twitter.consumerKey"), environment
+                .getProperty("twitter.consumerSecret")));
+        registry.addConnectionFactory(new VKontakteConnectionFactory(environment
+                .getProperty("vkontakte.clientId"), environment
+                .getProperty("vkontakte.clientSecret")));
+        return registry;
+    }
 
-	@Inject
-	private ThreadSecurityContextHolder securityContextHolder;
+    /**
+     * Singleton data access object providing access to connections across all
+     * users.
+     */
+    @Bean
+    public UsersConnectionRepository usersConnectionRepository() {
+        JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource,
+                connectionFactoryLocator(), Encryptors.noOpText());
+        repository.setConnectionSignUp(userSignUpAdapter());
+        return repository;
+    }
 
-	/**
-	 * When a new provider is added to the app register its {@link ConnectionFactory} here.
-	 */
-	@Bean
-	public ConnectionFactoryLocator connectionFactoryLocator() {
-		ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
-		registry.addConnectionFactory(new FacebookConnectionFactory(environment
-				.getProperty("facebook.clientId"), environment.getProperty("facebook.clientSecret")));
-		registry.addConnectionFactory(new TwitterConnectionFactory(environment
-				.getProperty("twitter.consumerKey"), environment
-				.getProperty("twitter.consumerSecret")));
-		registry.addConnectionFactory(new VKontakteConnectionFactory(environment
-				.getProperty("vkontakte.clientId"), environment
-				.getProperty("vkontakte.clientSecret")));
-		return registry;
-	}
+    @Bean
+    @Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+    public ConnectionRepository connectionRepository() {
+        Long userId = securityContextHolder.getContext().getUserId();
+        return userId != null ? usersConnectionRepository().createConnectionRepository(
+                userId.toString()) : null;
+    }
 
-	/**
-	 * Singleton data access object providing access to connections across all users.
-	 */
-	@Bean
-	public UsersConnectionRepository usersConnectionRepository() {
-		JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource,
-				connectionFactoryLocator(), Encryptors.noOpText());
-		repository.setConnectionSignUp(userSignUpAdapter());
-		return repository;
-	}
+    /**
+     * Creates a session and URL containing the session id to redirect to after
+     * a successful sign-in.
+     */
+    @Bean
+    public UserSignInAdapter userSignInAdapter() {
+        return new UserSignInAdapter();
+    }
 
-	@Bean
-	@Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
-	public ConnectionRepository connectionRepository() {
-		Long userId = securityContextHolder.getContext().getUserId();
-		return userId != null ? usersConnectionRepository().createConnectionRepository(
-				userId.toString()) : null;
-	}
+    /**
+     * Creates partially completed user and stores him in the database.
+     */
+    @Bean
+    public UserSignUpAdapter userSignUpAdapter() {
+        return new UserSignUpAdapter();
+    }
 
-	/**
-	 * Creates a session and URL containing the session id to redirect to after a successful
-	 * sign-in.
-	 */
-	@Bean
-	public UserSignInAdapter userSignInAdapter() {
-		return new UserSignInAdapter();
-	}
-
-	/**
-	 * Creates partially completed user and stores him in the database.
-	 */
-	@Bean
-	public UserSignUpAdapter userSignUpAdapter() {
-		return new UserSignUpAdapter();
-	}
-
-	/**
-	 * Provides helper methods for sign-in and connect controllers.
-	 */
-	@Bean
-	public ConnectSupport connectSupport() {
-		return new ConnectSupport();
-	}
+    /**
+     * Provides helper methods for sign-in and connect controllers.
+     */
+    @Bean
+    public ConnectSupport connectSupport() {
+        return new ConnectSupport();
+    }
 }
--- trunk/venefica-webapp/src/main/java/com/venefica/config/WebMvcConfig.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package com.venefica.config;
 
-
+import com.venefica.auth.TokenDecryptionInterceptorMvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
@@ -9,32 +9,30 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import com.venefica.auth.TokenDecryptionInterceptorMvc;
-
 /**
  * Spring MVC configuration
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public TokenDecryptionInterceptorMvc tokenDecryptionInterceptor() {
-		return new TokenDecryptionInterceptorMvc();
-	}
+    @Bean
+    public TokenDecryptionInterceptorMvc tokenDecryptionInterceptor() {
+        return new TokenDecryptionInterceptorMvc();
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(tokenDecryptionInterceptor());
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenDecryptionInterceptor());
+    }
 
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
-	}
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
