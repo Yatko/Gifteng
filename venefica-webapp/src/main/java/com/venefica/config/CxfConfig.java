bfbf02451abedcc9694834eaa96a6f25d139886e
Yatko <yatko@users.noreply.github.com>
2013-02-21 07:00:01 +0000

    reformat

--- venefica-webapp/src/main/java/com/venefica/config/CxfConfig.java
+++ venefica-webapp/src/main/java/com/venefica/config/CxfConfig.java
@@ -1,29 +1,27 @@
 package com.venefica.config;
 
-
+import com.venefica.auth.SecurityContextCleaner;
+import com.venefica.auth.TokenAuthorizationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-import com.venefica.auth.SecurityContextCleaner;
-import com.venefica.auth.TokenAuthorizationInterceptor;
-
 /**
  * Configuration of Apache CXF web services.
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Configuration
 @ImportResource("classpath:cxf-services.xml")
 public class CxfConfig {
 
-	@Bean(name = "securityContextCleaner")
-	public SecurityContextCleaner securityContextCleaner() {
-		return new SecurityContextCleaner();
-	}
+    @Bean(name = "securityContextCleaner")
+    public SecurityContextCleaner securityContextCleaner() {
+        return new SecurityContextCleaner();
+    }
 
-	@Bean(name = "tokenAuthorizationInterceptor")
-	public TokenAuthorizationInterceptor tokenAuthenticationInterceptor() {
-		return new TokenAuthorizationInterceptor();
-	}
+    @Bean(name = "tokenAuthorizationInterceptor")
+    public TokenAuthorizationInterceptor tokenAuthenticationInterceptor() {
+        return new TokenAuthorizationInterceptor();
+    }
 }
--- venefica-webapp/src/main/java/com/venefica/config/MainConfig.java
+++ venefica-webapp/src/main/java/com/venefica/config/MainConfig.java
@@ -1,19 +1,19 @@
 package com.venefica.config;
 
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
-
+import com.venefica.auth.ThreadSecurityContextHolder;
+import com.venefica.auth.TokenEncryptor;
+import com.venefica.dao.AdDao;
+import com.venefica.job.ExpirationJob;
 import java.security.NoSuchAlgorithmException;
-
 import javax.crypto.NoSuchPaddingException;
 import javax.inject.Inject;
-
-
+import static org.quartz.JobBuilder.newJob;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import org.quartz.Trigger;
+import static org.quartz.TriggerBuilder.newTrigger;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -22,67 +22,62 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
-import com.venefica.auth.ThreadSecurityContextHolder;
-import com.venefica.auth.TokenEncryptor;
-import com.venefica.dao.AdDao;
-import com.venefica.job.ExpirationJob;
-
 /**
  * Main configuration class for the application
- * 
+ *
  * @author Sviatoslav Grebenchukov
  */
 @Configuration
-@ComponentScan(basePackages = "com.venefica", excludeFilters = { @Filter(Configuration.class) })
+@ComponentScan(basePackages = "com.venefica", excludeFilters = {@Filter(Configuration.class)})
 @PropertySource("/application.properties")
 public class MainConfig {
 
-	public static final int EXPIRATION_INTERVAL_CHECK_SECS = 60;
+    public static final int EXPIRATION_INTERVAL_CHECK_SECS = 60;
+    
+    @Inject
+    private Environment environment;
+    
+    @Inject
+    private AdDao adDao;
 
-	@Inject
-	private Environment environment;
+    @Bean(name = "tokenEncryptor")
+    public TokenEncryptor tokenEncryptor() throws NoSuchAlgorithmException, NoSuchPaddingException {
+        return new TokenEncryptor(environment.getProperty("authentication.secretkey"));
+    }
 
-	@Bean(name = "tokenEncryptor")
-	public TokenEncryptor tokenEncryptor() throws NoSuchAlgorithmException, NoSuchPaddingException {
-		return new TokenEncryptor(environment.getProperty("authentication.secretkey"));
-	}
+    @Bean(name = "securityContextHolder")
+    public ThreadSecurityContextHolder threadSecurityContextHolder() {
+        return new ThreadSecurityContextHolder();
+    }
 
-	@Bean(name = "securityContextHolder")
-	public ThreadSecurityContextHolder threadSecurityContextHolder() {
-		return new ThreadSecurityContextHolder();
-	}
+    @Bean(name = "scheduler", destroyMethod = "shutdown")
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+        scheduler.scheduleJob(expirationJobDetail(), expirationTrigger());
+        scheduler.start();
+        return scheduler;
+    }
+    
+    @Bean(name = "expirationJobDetail")
+    public JobDetail expirationJobDetail() {
+        // @formatter:off
+        JobDetail job = newJob(ExpirationJob.class)
+                .withIdentity("expirationJob", "common")
+                .build();
+        job.getJobDataMap().put(ExpirationJob.ADDAO, adDao);
+        return job;
+        // @formatter:on 
+    }
 
-	@Bean(name = "scheduler", destroyMethod = "shutdown")
-	public Scheduler scheduler() throws SchedulerException {
-		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-		scheduler.scheduleJob(expirationJobDetail(), expirationTrigger());
-		scheduler.start();
-		return scheduler;
-	}
-
-	@Inject
-	private AdDao adDao;
-
-	@Bean(name = "expirationJobDetail")
-	public JobDetail expirationJobDetail() {
-		// @formatter:off
-		JobDetail job = newJob(ExpirationJob.class)
-				.withIdentity("expirationJob", "common")				
-				.build();
-		job.getJobDataMap().put(ExpirationJob.ADDAO, adDao);
-		return job;
-		// @formatter:on 
-	}
-
-	@Bean(name = "expirationTrigger")
-	public Trigger expirationTrigger() {
-		// @formatter:off
-		return newTrigger()
-				.withIdentity("expirationTrigger", "common")
-				.withSchedule(simpleSchedule()
-							.withIntervalInSeconds(EXPIRATION_INTERVAL_CHECK_SECS)
-							.repeatForever())
-				.build();
-		// @formatter:on
-	}
+    @Bean(name = "expirationTrigger")
+    public Trigger expirationTrigger() {
+        // @formatter:off
+        return newTrigger()
+                .withIdentity("expirationTrigger", "common")
+                .withSchedule(simpleSchedule()
+                .withIntervalInSeconds(EXPIRATION_INTERVAL_CHECK_SECS)
+                .repeatForever())
+                .build();
+        // @formatter:on
+    }
 }
