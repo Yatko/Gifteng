cca0b8d4bc28d6a7b5f859b95302a6bc3f4552d4
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-21 06:02:01 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/ProfileController.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/ProfileController.java
@@ -1,8 +1,11 @@
 package com.venefica;
 
+import com.venefica.auth.Token;
+import com.venefica.auth.TokenDecryptionException;
+import com.venefica.auth.TokenEncryptor;
+import com.venefica.dao.UserDao;
+import com.venefica.model.User;
 import javax.inject.Inject;
-
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -11,40 +14,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.venefica.auth.Token;
-import com.venefica.auth.TokenDecryptionException;
-import com.venefica.auth.TokenEncryptor;
-import com.venefica.dao.UserDao;
-import com.venefica.model.User;
-
 @Controller
 @RequestMapping("/profile")
 public class ProfileController {
 
-	private final Log log = LogFactory.getLog(ProfileController.class);
+    private final Log log = LogFactory.getLog(ProfileController.class);
+    
+    @Inject
+    private UserDao userDao;
+    
+    @Inject
+    TokenEncryptor tokenEncryptor;
 
-	@Inject
-	private UserDao userDao;
+    @RequestMapping(method = RequestMethod.GET)
+    public void profile(@RequestParam("token") String encryptedToken, Model model) {
+        try {
+            Token token = tokenEncryptor.decrypt(encryptedToken);
+            if (token != null) {
+                User user = userDao.get(token.getUserId());
 
-	@Inject
-	TokenEncryptor tokenEncryptor;
-
-	@RequestMapping(method = RequestMethod.GET)
-	public void profile(@RequestParam("token") String encryptedToken, Model model) {
-		try {
-			Token token = tokenEncryptor.decrypt(encryptedToken);
-			if (token != null) {
-				User user = userDao.get(token.getUserId());
-
-				if (user != null) {
-					model.addAttribute("token", encryptedToken);
-					model.addAttribute("firstName", user.getFirstName());
-					model.addAttribute("lastName", user.getLastName());
-					model.addAttribute("email", user.getEmail());
-				}
-			}
-		} catch (TokenDecryptionException e) {
-			log.warn(e);
-		}
-	}
+                if (user != null) {
+                    model.addAttribute("token", encryptedToken);
+                    model.addAttribute("firstName", user.getFirstName());
+                    model.addAttribute("lastName", user.getLastName());
+                    model.addAttribute("email", user.getEmail());
+                }
+            }
+        } catch (TokenDecryptionException e) {
+            log.warn(e);
+        }
+    }
 }
