214c33dd6c8dc8be1554216a161fd7ded9f57fde
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-21 06:16:07 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/service/dto/builder/MessageDtoBuilder.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/service/dto/builder/MessageDtoBuilder.java
@@ -6,39 +6,39 @@ import com.venefica.model.User;
 import com.venefica.service.dto.ImageDto;
 import com.venefica.service.dto.MessageDto;
 
-
 public class MessageDtoBuilder extends DtoBuilderBase<Message, MessageDto> {
-	private User currentUser;
 
-	public MessageDtoBuilder(Message model) {
-		super(model);
-	}
+    private User currentUser;
 
-	public MessageDtoBuilder setCurrentUser(User user) {
-		currentUser = user;
-		return this;
-	}
+    public MessageDtoBuilder(Message model) {
+        super(model);
+    }
 
-	@Override
-	public MessageDto build() {
-		MessageDto messageDto = new MessageDto();
-		messageDto.setId(model.getId());
-		messageDto.setText(model.getText());
+    public MessageDtoBuilder setCurrentUser(User user) {
+        currentUser = user;
+        return this;
+    }
 
-		boolean owner = currentUser != null ? currentUser.equals(model.getFrom()) : false;
-		messageDto.setOwner(owner);
+    @Override
+    public MessageDto build() {
+        MessageDto messageDto = new MessageDto();
+        messageDto.setId(model.getId());
+        messageDto.setText(model.getText());
 
-		Image toAvatar = model.getTo().getAvatar();
-		Image fromAvatar = model.getFrom().getAvatar();
-		messageDto.setToName(model.getTo().getName());
-		messageDto.setToFullName(model.getTo().getFullName());
-		messageDto.setToAvatarUrl(ImageDto.imageUrl(toAvatar));
-		messageDto.setFromName(model.getFrom().getName());
-		messageDto.setFromFullName(model.getFrom().getFullName());
-		messageDto.setFromAvatarUrl(ImageDto.imageUrl(fromAvatar));
-		messageDto.setRead(model.hasRead());
-		messageDto.setCreatedAt(model.getCreatedAt());
+        boolean owner = currentUser != null ? currentUser.equals(model.getFrom()) : false;
+        messageDto.setOwner(owner);
 
-		return messageDto;
-	}
+        Image toAvatar = model.getTo().getAvatar();
+        Image fromAvatar = model.getFrom().getAvatar();
+        messageDto.setToName(model.getTo().getName());
+        messageDto.setToFullName(model.getTo().getFullName());
+        messageDto.setToAvatarUrl(ImageDto.imageUrl(toAvatar));
+        messageDto.setFromName(model.getFrom().getName());
+        messageDto.setFromFullName(model.getFrom().getFullName());
+        messageDto.setFromAvatarUrl(ImageDto.imageUrl(fromAvatar));
+        messageDto.setRead(model.hasRead());
+        messageDto.setCreatedAt(model.getCreatedAt());
+
+        return messageDto;
+    }
 }
