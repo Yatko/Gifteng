f320d564013ab8d6211de3b7bd664d9b4a8c2af7
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-22 19:05:12 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/job/ExpirationJob.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/job/ExpirationJob.java
@@ -1,6 +1,5 @@
 package com.venefica.job;
 
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.quartz.DisallowConcurrentExecution;
@@ -13,20 +12,20 @@ import com.venefica.dao.AdDao;
 @DisallowConcurrentExecution
 public class ExpirationJob implements Job {
 
-	public static final String ADDAO = "AdDao";
+    public static final String ADDAO = "AdDao";
+    
+    private static final Log log = LogFactory.getLog(ExpirationJob.class);
 
-	private static final Log log = LogFactory.getLog(ExpirationJob.class);
+    @Override
+    // @Transactional
+    public void execute(JobExecutionContext ctx) throws JobExecutionException {
+        AdDao adDao = (AdDao) ctx.getJobDetail().getJobDataMap().get(ADDAO);
 
-	@Override
-	// @Transactional
-	public void execute(JobExecutionContext ctx) throws JobExecutionException {
-		AdDao adDao = (AdDao) ctx.getJobDetail().getJobDataMap().get(ADDAO);
-
-		try {
-			log.info("Processing expired advertisiments");
-			adDao.markExpiredAds();			
-		} catch (Exception e) {
-			log.error("Unable to process ads", e);
-		}
-	}
+        try {
+            log.info("Processing expired advertisiments");
+            adDao.markExpiredAds();
+        } catch (Exception e) {
+            log.error("Unable to process ads", e);
+        }
+    }
 }
