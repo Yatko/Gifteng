7fe9bd44cb0ec32018f3ab347c028789d7d0abc2
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-02-26 18:20:10 +0000

    reformat

--- trunk/venefica-webapp/src/main/java/com/venefica/service/package-info.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/service/package-info.java
@@ -1,8 +1,5 @@
 @XmlSchema(namespace = Namespace.SERVICE, attributeFormDefault = XmlNsForm.QUALIFIED, elementFormDefault = XmlNsForm.QUALIFIED)
 package com.venefica.service;
 
-import javax.xml.bind.annotation.XmlSchema;
 import javax.xml.bind.annotation.XmlNsForm;
-
-import com.venefica.service.Namespace;
-
+import javax.xml.bind.annotation.XmlSchema;
