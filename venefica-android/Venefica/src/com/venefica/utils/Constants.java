27cfb8266e7a2d3877a5bad9aa4d250a8ee024b8
avinash.yatko <avinash@venefica.labs>
2013-02-26 20:11:52 +0000

    constant added for expiration days

--- venefica-android/Venefica/src/com/venefica/utils/Constants.java
+++ venefica-android/Venefica/src/com/venefica/utils/Constants.java
@@ -164,4 +164,6 @@ public class Constants
 	public static final String PREF_DEF_VAL_MIN_PRICE = "0";
 	public static final int PREF_DEF_VAL_CATEGORY = -1;	
 	
+	public static final int EXPIRE_AD_IN_DAYS = 60;	
+	
 }
