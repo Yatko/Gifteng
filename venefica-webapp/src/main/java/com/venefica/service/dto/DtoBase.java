9c788c6ad421619162be0ceb22e69aebbb83d5a6
Yatko <yatko@users.noreply.github.com>
2013-03-11 06:30:39 +0000

    committing everything

--- venefica-webapp/src/main/java/com/venefica/service/dto/DtoBase.java
+++ venefica-webapp/src/main/java/com/venefica/service/dto/DtoBase.java
@@ -3,11 +3,13 @@ package com.venefica.service.dto;
 import javax.xml.bind.annotation.XmlAttribute;
 
 public class DtoBase {
-
+    
     @SuppressWarnings("unused")
     //NOTE: why was this namespace set?
-    //@XmlAttribute(name = "type", namespace = "http://www.w3.org/2001/XMLSchema-instance")
-    @XmlAttribute(name = "type")
+    //without this namespace the android app cannot parse WS result
+    @XmlAttribute(name = "type", namespace = "http://www.w3.org/2001/XMLSchema-instance")
+    //@XmlAttribute(name = "type", namespace = com.venefica.service.Namespace.DTO)
+    //@XmlAttribute(name = "type")
     private String type;
 
     public DtoBase() {
