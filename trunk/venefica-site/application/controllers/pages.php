85e45185bac25aaa4b85eac6e6f9e1621703b262
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-03-15 13:14:08 +0000

    - the first full commit of the website - not yet implemented anything

---
+++ trunk/venefica-site/application/controllers/pages.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pages extends CI_Controller {
+
+    public function view($page = 'home') {
+        if (!file_exists('application/views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/' . $page, $data);
+        $this->load->view('templates/footer', $data);
+    }
+
+}
