f1b9a8051a7533a10ef0ae19aa39f3ab60464a16
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-03-19 16:56:06 +0000

    header and footer changes

--- trunk/venefica-site/application/views/templates/header.php
+++ trunk/venefica-site/application/views/templates/header.php
@@ -1,6 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Gifteng</title>
+    <title><?=lang('main_title')?></title>
+    
+    <meta charset="utf-8"/>
+    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+    
+    <link rel="shortcut icon" href="assets/img/favicon.ico" >
+    
+    <link rel='stylesheet' type='text/css' media='all' href="assets/css/style.css" />
+    <link rel='stylesheet' type='text/css' media='all' href="assets/css/tabs.css" />
+    
+    <script src="assets/js/jquery-1.9.1.min.js"></script>
+    <script src="assets/js/jquery-easing.1.2.js"></script>
+    <script src="assets/js/jquery-easing-compatibility.1.2.js"></script>
+    <script src="assets/js/coda-slider.1.1.1.js"></script>
+    <script src="assets/js/textbox-hint.js"></script>
+    
+    <script language="javascript">
+        $(function() {
+            // find all the input elements with title attributes
+            $('input[title != ""]').hint();
+        });
+    </script>
 </head>
 <body>
+    <div id="header">
+        <div>
+            <div class="logo"></div>
+            <div class="slogan"></div>
+        </div>
+    </div> <!-- end header -->
+    
+    <div id="container">
