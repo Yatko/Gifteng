b7077d4344b8f0a6cb0d4492a88b6e3aab37736c
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-03-19 16:54:43 +0000

    language files

---
+++ trunk/venefica-site/application/language/english/authentication_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['authentication_tab_invitation'] = "Invitation";
+$lang['authentication_tab_login'] = "Log In";
+
+$lang['authentication_login_email_hint'] = "Email adress";
+$lang['authentication_login_password_hint'] = "Password";
+$lang['authentication_login_submit_button'] = "LOG IN";
+
---
+++ trunk/venefica-site/application/language/english/main_lang.php
@@ -0,0 +1,3 @@
+<?php
+
+$lang['main_title'] = "Gifteng";
