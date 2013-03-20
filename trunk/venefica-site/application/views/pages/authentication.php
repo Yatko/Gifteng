1389aad4e24593b88bd36f66c110eaa7f393338c
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-03-20 12:38:05 +0000

    translations

--- trunk/venefica-site/application/views/pages/authentication.php
+++ trunk/venefica-site/application/views/pages/authentication.php
@@ -10,17 +10,27 @@
             <div class="panel" title="<?=lang('authentication_tab_invitation')?>">
                 <div class="wrapper">
                     <form>
-                        <div></div>
+                        <div><input type="text" class="textbox" title="<?=lang('authentication_invitation_email_hint')?>" placeholder="<?=lang('authentication_invitation_email_hint')?>"></div>
+                        <div><a href="#" class="buttonBlue"><?=lang('authentication_invitation_request_button')?></a></div>
                     </form>
+                    <div><a href="#" class="linkOrange"><?=lang('authentication_invitation_invitation')?></a></div>
                 </div>
             </div>
 
             <div class="panel" title="<?=lang('authentication_tab_login')?>">
                 <div class="wrapper">
                     <form>
-                        <div><input type="text" title="<?=lang('authentication_login_email_hint')?>" placeholder="<?=lang('authentication_login_email_hint')?>"></div>
-                        <div><input type="password" title="<?=lang('authentication_login_password_hint')?>" placeholder="<?=lang('authentication_login_password_hint')?>"></div>
-                        <div><a href="#" class="buttonBlue"><?=lang('authentication_login_submit_button')?></a></div>
+                        <div><input type="text" class="textbox" title="<?=lang('authentication_login_email_hint')?>" placeholder="<?=lang('authentication_login_email_hint')?>"></div>
+                        <div><input type="password" class="textbox" title="<?=lang('authentication_login_password_hint')?>" placeholder="<?=lang('authentication_login_password_hint')?>"></div>
+                        <div><a href="#" class="buttonBlue"><?=lang('authentication_login_login_button')?></a></div>
+                        <div class="tabContent">
+                            <div class="fl"><?=lang('authentication_login_forgot_password')?></div>
+                            <div class="fr">
+                                <input id="remember_me" type="checkbox" class="checkbox">
+                                <label for="remember_me"><?=lang('authentication_login_remember_me')?></label>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
                     </form>
                 </div>
             </div>
