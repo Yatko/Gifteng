6ac750f305805f694c2a0cc246cc8f821d89e9a2
Yatko <yatko@users.noreply.github.com>
2013-03-22 12:26:53 +0000

    autoloading form helper

--- venefica-site/application/config/autoload.php
+++ venefica-site/application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'language');
+$autoload['helper'] = array('url', 'language', 'form');
 
 
 /*
