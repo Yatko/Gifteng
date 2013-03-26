a69c834f4630effccd4a5985ad199b3006487b60
Yatko <yatko@users.noreply.github.com>
2013-03-26 07:13:01 +0000

    some translations

--- venefica-site/application/language/english/authentication_lang.php
+++ venefica-site/application/language/english/authentication_lang.php
@@ -6,6 +6,8 @@ $lang['authentication_tab_login'] = "Log In";
 $lang['authentication_invitation_email_hint'] = "Email adress";
 $lang['authentication_invitation_request_button'] = "REQUEST AN INVITATION";
 $lang['authentication_invitation_invitation'] = "I have an invitation";
+$lang['authentication_invitation_code_hint'] = "Enter your invitation code";
+$lang['authentication_invitation_verify_button'] = "VERIFY INVITATION";
 
 $lang['authentication_login_email_hint'] = "Email adress";
 $lang['authentication_login_password_hint'] = "Password";
@@ -15,8 +17,15 @@ $lang['authentication_login_remember_me'] = "Remember me";
 
 $lang['authentication_login_welcome'] = "Welcome <b>%s</b>!";
 
+#
+#invitation form fields
+#
 $lang['invitation_email'] = "Email";
+$lang['invitation_code'] = "Invitation code";
 
+#
+#login form fields
+#
 $lang['authentication_email'] = "Email";
 $lang['authentication_password'] = "Password";
 $lang['authentication_remember_me'] = "Remember me";
