c129390e1b8286a9858ad2b8226adb9f7cedef59
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-03-31 18:23:25 +0000

    enum for member user data

---
+++ trunk/venefica-webapp/src/main/java/com/venefica/model/MaritalStatus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.venefica.model;
+
+/**
+ *
+ * @author gyuszi
+ */
+public enum MaritalStatus {
+
+    SINGLE,
+    MARRIED,
+    DIVORCED,
+    //... more to come
+}
