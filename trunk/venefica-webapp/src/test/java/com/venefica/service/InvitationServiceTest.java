6421f48080ae5d66430006097c91ed1fb21a39af
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-04-01 21:11:53 +0000

    - unit test added - this should be improved

--- trunk/venefica-webapp/src/test/java/com/venefica/service/InvitationServiceTest.java
+++ trunk/venefica-webapp/src/test/java/com/venefica/service/InvitationServiceTest.java
@@ -9,7 +9,7 @@ import com.venefica.model.Invitation;
 import com.venefica.model.UserType;
 import com.venefica.service.dto.InvitationDto;
 import javax.inject.Inject;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,6 +24,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ActiveProfiles("test")
 @ContextConfiguration(locations = "/InvitationServiceTest-context.xml")
 public class InvitationServiceTest extends ServiceTestBase<InvitationService> {
+    
+    private static final String TEST_INVITATION_CODE = "12345";
     
     @Inject
     private InvitationDao invitationDao;
@@ -46,5 +48,11 @@ public class InvitationServiceTest extends ServiceTestBase<InvitationService> {
         Invitation invitation = invitationDao.get(invitationId);
         assertNotNull("The invitation with id = " + invitationId + " not found!", invitation);
         assertNotNull("Expiration date must be set!", invitation.getExpiresAt());
+    }
+    
+    @Test
+    public void isInvitationValidTest() {
+        boolean valid = client.isInvitationValid(TEST_INVITATION_CODE);
+        assertTrue("The invitation with code = " + TEST_INVITATION_CODE + " should be valid", valid);
     }
 }
