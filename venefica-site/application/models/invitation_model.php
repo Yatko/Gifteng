a1254435b9d9262a1bbece92a9151b4bed31c6aa
Yatko <yatko@users.noreply.github.com>
2013-04-01 20:41:59 +0000

    should be overriden the __get function to fix the $type invocation that generate a PHP notice

--- venefica-site/application/models/invitation_model.php
+++ venefica-site/application/models/invitation_model.php
@@ -12,6 +12,17 @@ class Invitation_model extends CI_Model {
     var $source;
     var $userType;
     
+    public function __construct() {
+        // Call the Model constructor
+        parent::__construct();
+    }
+    
+    public function __get($key) {
+        //the following is queried by the SoapClient
+        if ( $key == "type" ) return "Invitation";
+        return parent::__get($key);
+    }
+    
     public function toString() {
         return "Invitation ["
             ."email=".$this->email.", "
