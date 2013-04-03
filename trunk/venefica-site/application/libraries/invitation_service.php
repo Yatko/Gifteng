d40b0cf5497b351aef6d7e08b9f5b65a859e8255
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-04-03 21:05:28 +0000

    - new WS methods ported - form validator

--- trunk/venefica-site/application/libraries/invitation_service.php
+++ trunk/venefica-site/application/libraries/invitation_service.php
@@ -11,7 +11,7 @@ class Invitation_service {
      * Requests an invitation from the server.
      * 
      * @param Invitation_model the invitation model
-     * @return the invitation ID
+     * @return long the invitation ID
      * @throws Exception in case of WS invocation error
      */
     public function requestInvitation($invitation) {
@@ -22,6 +22,26 @@ class Invitation_service {
             return $invitationId;
         } catch ( Exception $ex ) {
             log_message(INFO, 'Invitation request failed! '.$ex->faultstring);
+            throw new Exception($ex->faultstring);
+        }
+    }
+    
+    /**
+     * Validates the given invitation code. If validation pass the flow can continue
+     * with user registrtation
+     * 
+     * @param string $code the invitation code
+     * @return boolean true if the given invitation code exists and is valid
+     * @throws Exception
+     */
+    public function isInvitationValid($code) {
+        try {
+            $invitationService = new SoapClient(INVITATION_SERVICE_WSDL, getSoapOptions());
+            $result = $invitationService->isInvitationValid(array("code" => $code));
+            $valid = $result->valid;
+            return $valid;
+        } catch ( Exception $ex ) {
+            log_message(INFO, 'Invitation validation failed! '.$ex->faultstring);
             throw new Exception($ex->faultstring);
         }
     }
