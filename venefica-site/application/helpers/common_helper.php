7f65ba75b372b653602d4a7f132884ef409e689c
Yatko <yatko@users.noreply.github.com>
2013-04-08 18:14:37 +0000

    more helper methods

--- venefica-site/application/helpers/common_helper.php
+++ venefica-site/application/helpers/common_helper.php
@@ -15,9 +15,21 @@ if ( ! function_exists('display')) {
 
 if ( ! function_exists('getField')) {
     function getField($obj, $fieldName) {
+        if ( $obj == null || $fieldName == null ) {
+            return null;
+        }
         if ( property_exists($obj, $fieldName) ) {
             return $obj->$fieldName;
         }
         return null;
+    }
+}
+
+if ( ! function_exists('hasField')) {
+    function hasField($obj, $fieldName) {
+        if ( $obj == null || $fieldName == null ) {
+            return FALSE;
+        }
+        return property_exists($obj, $fieldName);
     }
 }
