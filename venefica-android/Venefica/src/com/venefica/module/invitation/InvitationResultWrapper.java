d401c064e008c7f14eb2b534ba23601202db1fa5
avinash.yatko <avinash@venefica.labs>
2013-04-08 21:36:10 +0000

    wrapper class to hold invitation result

---
+++ venefica-android/Venefica/src/com/venefica/module/invitation/InvitationResultWrapper.java
@@ -0,0 +1,10 @@
+package com.venefica.module.invitation;
+
+/**
+ * @author avinash
+ * Wrapper class for Invitation result data 
+ */
+public class InvitationResultWrapper {
+	public int result = -1;
+	public String data = null;
+}
