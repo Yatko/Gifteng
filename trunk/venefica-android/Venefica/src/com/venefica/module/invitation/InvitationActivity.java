db96cf2a12b277da42fbf81e256dc39d36f327ed
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-04-09 18:38:22 +0000

    Implemented business logic and web service integration for verify invitation and register user functionality

--- trunk/venefica-android/Venefica/src/com/venefica/module/invitation/InvitationActivity.java
+++ trunk/venefica-android/Venefica/src/com/venefica/module/invitation/InvitationActivity.java
@@ -21,6 +21,8 @@ import com.venefica.module.invitation.VerifyInvitationFragment.OnVerifyInvitatio
 import com.venefica.module.invitation.VerifyInvitationSuccessFragment.OnVerifyInvitationSuccessClickListener;
 import com.venefica.module.main.R;
 import com.venefica.module.network.WSAction;
+import com.venefica.module.user.UserDto;
+import com.venefica.module.user.UserRegistrationResultWrapper;
 import com.venefica.module.utils.Utility;
 import com.venefica.services.InvitationDto;
 import com.venefica.services.InvitationDto.UserType;
@@ -58,9 +60,30 @@ public class InvitationActivity extends SherlockFragmentActivity
 	 */
 	private int ERROR_CODE;
 	private WSAction wsAction;
+	/**
+	 * to request invitation
+	 */
 	private InvitationDto invitationDto;
+	/**
+	 * email to request invitation
+	 */
 	private String email = "";
-	public boolean uploadingData = false;
+	/**
+	 * true if working in background
+	 */
+	private boolean uploadingData = false;
+	/**
+	 * Invitation code to verify
+	 */
+	private String invitationCode = ""; 
+	/**
+	 * password
+	 */
+	private String password = "";
+	/**
+	 * User to register
+	 */
+	private UserDto userDto;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -109,7 +132,7 @@ public class InvitationActivity extends SherlockFragmentActivity
 				public void onClick(DialogInterface dialog, int which) {
 					dismissDialog(D_ERROR);
 					if(ERROR_CODE == Constants.RESULT_REGISTER_USER_SUCCESS){
-//						RegisterUserActivity.this.finish();
+						InvitationActivity.this.finish();
 					}
 				}
 			});			
@@ -127,8 +150,18 @@ public class InvitationActivity extends SherlockFragmentActivity
     			message = (String) getResources().getText(R.string.error_network_01);
 			} else if(ERROR_CODE == Constants.ERROR_NETWORK_CONNECT){
 				message = (String) getResources().getText(R.string.error_network_02);
-			}else if(ERROR_CODE == Constants.ERROR_RESULT_REQ_INVITATION){
+			} else if(ERROR_CODE == Constants.ERROR_RESULT_REQ_INVITATION){
 				message = (String) getResources().getText(R.string.g_error_req_invtation_code);
+			} else if(ERROR_CODE == Constants.ERROR_RESULT_VERIFY_INVITATION){
+				message = (String) getResources().getText(R.string.g_error_invtation_code);
+			} else if(ERROR_CODE == Constants.ERROR_RESULT_REGISTER_USER){
+				message = (String) getResources().getText(R.string.error_register_user);
+			} else if(ERROR_CODE == Constants.RESULT_REGISTER_USER_DUP_EMAIL){
+				message = (String) getResources().getText(R.string.msg_reg_user_duplicate_email);
+			} else if(ERROR_CODE == Constants.RESULT_REGISTER_USER_DUP_LOGIN){
+				message = (String) getResources().getText(R.string.msg_reg_user_duplicate_login);
+			} else if(ERROR_CODE == Constants.RESULT_REGISTER_USER_SUCCESS){
+				message = (String) getResources().getText(R.string.msg_reg_user_registration_success);
 			}
     		((AlertDialog) dialog).setMessage(message);
 		}    	
@@ -138,17 +171,25 @@ public class InvitationActivity extends SherlockFragmentActivity
 	 */
 	@Override
 	public void onConfirmRequest(String zipCode, String source, UserType useType) {
-		if (!uploadingData) {
-			if (invitationDto == null) {
-				invitationDto = new InvitationDto();
+		if (WSAction.isNetworkConnected(this)) {
+			if (!uploadingData) {
+				if (invitationDto == null) {
+					invitationDto = new InvitationDto();
+				}
+				invitationDto.setEmail(email);
+				invitationDto.setSource(source);
+				invitationDto.setZipCode(zipCode);
+				invitationDto.setUserType(useType);
+				new InvitationTask().execute(ACT_MODE_CONF_INVITATION_REQ);
+			} else {
+				Utility.showLongToast(
+						this,
+						getResources().getString(
+								R.string.msg_working_in_background));
 			}
-			invitationDto.setEmail(email);
-			invitationDto.setSource(source);
-			invitationDto.setZipCode(zipCode);
-			invitationDto.setUserType(useType);
-			new InvitationTask().execute(ACT_MODE_CONF_INVITATION_REQ);
 		} else {
-			Utility.showLongToast(this, getResources().getString(R.string.msg_working_in_background));
+			ERROR_CODE = Constants.ERROR_NETWORK_UNAVAILABLE;
+			showDialog(D_ERROR);
 		}
 	}
 	/* (non-Javadoc)
@@ -156,15 +197,29 @@ public class InvitationActivity extends SherlockFragmentActivity
 	 */
 	@Override
 	public void OnVerifyInvitation(String invitationCode) {
-		VerifyInvitationSuccessFragment invitationSuccessFragment = new VerifyInvitationSuccessFragment();
-		getSupportFragmentManager().beginTransaction().replace(R.id.layActInvitationRoot, invitationSuccessFragment).commit();
+		this.invitationCode = invitationCode;
+		new InvitationTask().execute(ACT_MODE_VERIFY_INVTATION);
 	}
 	/* (non-Javadoc)
 	 * @see com.venefica.module.invitation.JoinGiftingFragment.OnJoinGiftingFragmentListener#onJoinButtonClick()
 	 */
 	@Override
-	public void onJoinButtonClick() {
-		
+	public void onJoinButtonClick(String password, UserDto userDto) {
+		if (WSAction.isNetworkConnected(this)) {
+			if (!uploadingData) {
+				this.password = password;
+				this.userDto = userDto;
+			new RegisterUserTask().execute(ACT_MODE_JOIN_GIFTENG);
+			} else {
+				Utility.showLongToast(
+						this,
+						getResources().getString(
+								R.string.msg_working_in_background));
+			}
+		} else {
+			ERROR_CODE = Constants.ERROR_NETWORK_UNAVAILABLE;
+			showDialog(D_ERROR);
+		}
 	}
 	/* (non-Javadoc)
 	 * @see com.venefica.module.invitation.VerifyInvitationSuccessFragment.OnVerifyInvitationSuccessClickListener#OnGotoJoinClick()
@@ -195,6 +250,8 @@ public class InvitationActivity extends SherlockFragmentActivity
 				}
 				if (params[0].equals(ACT_MODE_CONF_INVITATION_REQ)) {
 					wrapper = wsAction.requestInvitation(((VeneficaApplication)getApplication()).getAuthToken(), invitationDto);
+				} else if (params[0].equals(ACT_MODE_VERIFY_INVTATION)) {
+					wrapper = wsAction.verifyInvitation(((VeneficaApplication)getApplication()).getAuthToken(), invitationCode);
 				}
 			}catch (IOException e) {
 				Log.e("InvitationTask::doInBackground :", e.toString());
@@ -212,12 +269,68 @@ public class InvitationActivity extends SherlockFragmentActivity
 			if(result.data == null && result.result == -1){
 				ERROR_CODE = Constants.ERROR_NETWORK_CONNECT;
 				showDialog(D_ERROR);
-			} else if (result.result == Constants.ERROR_RESULT_REQ_INVITATION) {
+			} else if (result.result == Constants.ERROR_RESULT_REQ_INVITATION ) {
 				ERROR_CODE = result.result;
 				showDialog(D_ERROR);
+			} else if (result.result == Constants.ERROR_RESULT_VERIFY_INVITATION) {
+				if (verifyInvitationFragment != null) {
+					verifyInvitationFragment.showInvalidCodeError(true);
+				}
 			} else if (result.result == Constants.RESULT_REQ_INVITATION_SUCCESS) {
+				//Show success message
 				RequestInvitationSuccessFragment requestInvitationSuccessFragment = new RequestInvitationSuccessFragment();
-				getSupportFragmentManager().beginTransaction().replace(R.id.layActInvitationRoot, requestInvitationSuccessFragment).commit();
+				getSupportFragmentManager().beginTransaction()
+					.replace(R.id.layActInvitationRoot, requestInvitationSuccessFragment).commit();
+			} else if (result.result == Constants.RESULT_VIFY_INVITATION_SUCCESS) {
+				if (verifyInvitationFragment != null) {					
+					verifyInvitationFragment.showInvalidCodeError(false);
+				}
+				//Show success message
+				VerifyInvitationSuccessFragment invitationSuccessFragment = new VerifyInvitationSuccessFragment();
+				getSupportFragmentManager().beginTransaction()
+					.replace(R.id.layActInvitationRoot, invitationSuccessFragment).commit();
+			}
+		}
+	}
+	
+	/**
+	 * @author avinash
+	 * Class to perform network task.
+	 */
+	class RegisterUserTask extends AsyncTask<Integer, Integer, UserRegistrationResultWrapper>{
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			setSupportProgressBarIndeterminateVisibility(true);
+		}
+		@Override
+		protected UserRegistrationResultWrapper doInBackground(Integer... params) {
+			UserRegistrationResultWrapper wrapper = new UserRegistrationResultWrapper();
+			try {
+				if(wsAction == null ){
+					wsAction = new WSAction();
+				}
+				if (params[0].equals(ACT_MODE_JOIN_GIFTENG)) {					
+					wrapper.result = wsAction.registerUser(password, userDto, invitationCode);					
+				}
+			}catch (IOException e) {
+				Log.e("AuthenticationTask::doInBackground :", e.toString());
+				wrapper.result = Constants.ERROR_NETWORK_CONNECT;
+			} catch (XmlPullParserException e) {
+				Log.e("AuthenticationTask::doInBackground :", e.toString());
+			}
+			return wrapper;
+		}
+		@Override
+		protected void onPostExecute(UserRegistrationResultWrapper result) {
+			super.onPostExecute(result);
+			setSupportProgressBarIndeterminateVisibility(false);
+			if(result.userDto == null && result.result == -1){
+				ERROR_CODE = Constants.ERROR_NETWORK_CONNECT;
+				showDialog(D_ERROR);
+			}else if (result.result != -1) {
+				ERROR_CODE = result.result;
+				showDialog(D_ERROR);				
 			}
 		}
 	}
