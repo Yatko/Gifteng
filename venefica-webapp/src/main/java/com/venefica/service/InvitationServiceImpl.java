a2ea8d400ebe3f9a24ba27e311fe9927f5f6bfea
Yatko <yatko@users.noreply.github.com>
2013-04-13 15:29:31 +0000

    - generating mail based on velocity templates - sending mail at invitation request

--- venefica-webapp/src/main/java/com/venefica/service/InvitationServiceImpl.java
+++ venefica-webapp/src/main/java/com/venefica/service/InvitationServiceImpl.java
@@ -4,24 +4,32 @@
  */
 package com.venefica.service;
 
+import com.venefica.common.EmailSender;
+import com.venefica.config.Constants;
 import com.venefica.dao.InvitationDao;
 import com.venefica.model.Invitation;
 import com.venefica.service.dto.InvitationDto;
+import com.venefica.service.fault.InvitationException;
+import java.io.StringWriter;
 import java.util.Date;
-import javax.annotation.Resource;
+import java.util.Properties;
+import javax.annotation.PostConstruct;
 //import java.util.UUID;
 import javax.inject.Inject;
 import javax.jws.WebService;
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.ws.WebServiceContext;
-import javax.xml.ws.handler.MessageContext;
 import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateUtils;
-import org.apache.cxf.message.Message;
-import org.apache.cxf.phase.PhaseInterceptorChain;
-import org.apache.cxf.transport.http.AbstractHTTPDestination;
+import org.apache.commons.mail.EmailException;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.exception.TemplateInitException;
+import org.apache.velocity.runtime.RuntimeConstants;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -32,27 +40,87 @@ import org.springframework.stereotype.Service;
 public class InvitationServiceImpl extends AbstractService implements InvitationService {
 
     private static final int EXPIRATION_PERIOD_DAYS = 3;
-    private static final int CODE_LENGTH = 8;
+    private static final int CODE_LENGTH = 4;
     
-    @Resource
-    private WebServiceContext jaxwsContext;
+    private static final String TEMPLATES_FOLDER = "templates/";
+    private static final String INVITATION_REQUEST_SUBJECT_TEMPLATE = TEMPLATES_FOLDER + "invitation-request-subject.vm";
+    private static final String INVITATION_REQUEST_HTML_MESSAGE_TEMPLATE = TEMPLATES_FOLDER + "invitation-request-message.html.vm";
+    private static final String INVITATION_REQUEST_PLAIN_MESSAGE_TEMPLATE = TEMPLATES_FOLDER + "invitation-request-message.txt.vm";
     
     @Inject
     private InvitationDao invitationDao;
+    @Inject
+    private EmailSender emailSender;
+    
+    private VelocityEngine velocityEngine;
+    
+    @PostConstruct
+    public void init() {
+        try {
+            Properties props = new Properties();
+            props.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "org.apache.velocity.runtime.log.Log4JLogChute");
+            props.setProperty("runtime.log.logsystem.log4j.category", "velocity");
+            props.setProperty("runtime.log.logsystem.log4j.logger", "velocity");
+            props.setProperty(RuntimeConstants.RESOURCE_LOADER, "class");
+            props.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            props.setProperty(RuntimeConstants.INPUT_ENCODING, Constants.DEFAULT_CHARSET);
+            props.setProperty(RuntimeConstants.OUTPUT_ENCODING, Constants.DEFAULT_CHARSET);
+            
+            for ( String name : props.stringPropertyNames() ) {
+                String value = props.getProperty(name);
+                logger.debug("Setting property for velocity initialization (" + name + "=" + value + ")");
+            }
+            
+            velocityEngine = new VelocityEngine(props);
+            velocityEngine.init();
+            logger.info("Velocity engine initialized");
+        } catch ( Exception ex ) {
+            logger.error(ex.getClass().getSimpleName() + " thrown when trying to initialize the velocity engine", ex);
+        }
+    }
     
     @Override
-    public Long requestInvitation(InvitationDto invitationDto) {
+    @Transactional
+    public Long requestInvitation(InvitationDto invitationDto) throws InvitationException {
         Invitation invitation = new Invitation();
         invitationDto.update(invitation);
         
-        String code = generateCode();
+        String code;
+        int generationTried = 0;
+        while ( true ) {
+            code = generateCode();
+            generationTried++;
+            if ( invitationDao.findByCode(code) == null ) {
+                //the generated code does not exists, found an unused (free) one
+                break;
+            } else if ( generationTried >= 10 ) {
+                throw new InvitationException("Cannot generate valid invitation code!");
+            }
+        }
+        
         Date expiresAt = DateUtils.addDays(new Date(), EXPIRATION_PERIOD_DAYS);
         invitation.setExpiresAt(expiresAt);
         invitation.setExpired(false);
         invitation.setCode(code);
         invitation.setIpAddress(getIpAddress());
         
-        return invitationDao.save(invitation);
+        Long invitationId = invitationDao.save(invitation);
+        
+        try {
+            String subject = mergeVelocityTemplate(INVITATION_REQUEST_SUBJECT_TEMPLATE, code);
+            String htmlMessage = mergeVelocityTemplate(INVITATION_REQUEST_HTML_MESSAGE_TEMPLATE, code);
+            String plainMessage = mergeVelocityTemplate(INVITATION_REQUEST_PLAIN_MESSAGE_TEMPLATE, code);
+            
+            emailSender.sendHtmlEmail(subject, htmlMessage, plainMessage, invitationDto.getEmail());
+        } catch ( EmailException ex ) {
+            logger.error("Email exception", ex);
+            throw new InvitationException("Could not send invitation mail!");
+        } catch ( RuntimeException ex ) {
+            logger.error("Runtime exception", ex);
+            throw new InvitationException(ex.getMessage());
+        }
+        
+        return invitationId;
     }
     
     @Override
@@ -74,32 +142,33 @@ public class InvitationServiceImpl extends AbstractService implements Invitation
     
     private String generateCode() {
         //String code = UUID.randomUUID().toString();
-        String code = RandomStringUtils.randomAlphanumeric(CODE_LENGTH);
+        String code = RandomStringUtils.randomNumeric(CODE_LENGTH);
         return code;
     }
-    
-    private String getIpAddress() { 
-        String ipAddress = null;
-        HttpServletRequest request;
-        String header = "X-Forwarded-For";
+
+    private String mergeVelocityTemplate(String templateName, String invitationCode) {
+        if ( velocityEngine == null ) {
+            throw new RuntimeException("Velocity engine is not initialized");
+        }
         
-        if ( jaxwsContext != null ) {
-            request = (HttpServletRequest) jaxwsContext.getMessageContext().get(MessageContext.SERVLET_REQUEST);
-        } else {
-            Message message = PhaseInterceptorChain.getCurrentMessage();
-            request = (HttpServletRequest) message.get(AbstractHTTPDestination.HTTP_REQUEST);
-        }
+        try {
+            Template template = velocityEngine.getTemplate(templateName);
+            VelocityContext context = new VelocityContext();
+            context.put("invitationCode", invitationCode);
 
-        if ( request != null ) {
-            if ( StringUtils.isNotBlank(request.getHeader(header)) ) {
-                ipAddress = request.getHeader(header);
-            } else {
-                ipAddress = request.getRemoteAddr();
-            }
+            StringWriter sw = new StringWriter();
+            template.merge(context, sw);
+            return sw.toString();
+        } catch ( ResourceNotFoundException ex ) {
+            throw new RuntimeException("Resource (" + templateName + ") not found", ex);
+        } catch ( ParseErrorException ex ) {
+            throw new RuntimeException("Syntax error! Problem parsing resource (" + templateName + ")", ex);
+        } catch ( MethodInvocationException ex ) {
+            throw new RuntimeException("Invocation error on resource (" + templateName + ")", ex);
+        } catch ( TemplateInitException ex ) {
+            throw new RuntimeException("Initialization error of resource (" + templateName + ")", ex);
+        } catch ( Exception ex ) {
+            throw new RuntimeException("Exception thrown on resource (" + templateName + "): " + ex.getMessage(), ex);
         }
-
-        logger.info("IP ADDRESS: " + ipAddress);
-        return ipAddress; 
     }
-    
 }
