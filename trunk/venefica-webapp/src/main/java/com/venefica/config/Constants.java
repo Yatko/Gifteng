938380ccfb549ab64f02564752b7b23816d1fae0
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-04-13 11:07:07 +0000

    some more constants

--- trunk/venefica-webapp/src/main/java/com/venefica/config/Constants.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/config/Constants.java
@@ -14,6 +14,8 @@ import java.util.Set;
  */
 public interface Constants {
     
+    public static final String MODEL_PACKAGE = "com.venefica.model";
+    
     public static final String MESSAGE_SIGNATURE = "MessageSig";
     public static final String AUTH_TOKEN = "AuthToken";
     
@@ -22,6 +24,8 @@ public interface Constants {
     
     public static final String AD_DAO = "AdDao";
     public static final String INVITATION_DAO = "InvitationDao";
+    
+    public static final String DEFAULT_CHARSET = "UTF-8";
     
     public static final Set<String> OPERATIONS_FOR_SKIP_TOKEN_AUTHORIZATION = new HashSet<String>(Arrays.asList(
             //registration related
