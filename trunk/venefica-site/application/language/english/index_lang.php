a7f1d9e1a1fd35ad1a064876f0ade38a0a2e2bab
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-04-22 09:51:02 +0000

    translations

--- trunk/venefica-site/application/language/english/index_lang.php
+++ trunk/venefica-site/application/language/english/index_lang.php
@@ -3,11 +3,5 @@
 $lang['index_welcome'] = '<strong>Gifteng</strong> is an invitation-only, social community<br />where Members can give and receive things they love for free';
 $lang['index_slogan'] = "&middot;&nbsp; make the world a giving place &nbsp;&middot;";
 
-$lang['index_invitation_email_hint'] = "Email address";
-$lang['index_invitation_request_button'] = "REQUEST AN INVITATION";
-
 $lang['invitation_invitation_invitation'] = "I have an invitation";
 $lang['invitation_invitation_request'] = "Request an invitation";
-
-$lang['index_invitation_code_hint'] = "Enter your invitation code";
-$lang['index_invitation_verify_button'] = "VERIFY INVITATION";
