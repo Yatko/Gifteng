c0c45075529e6c77edbde8668d9e98df027070b2
Yatko <yatko@users.noreply.github.com>
2013-05-01 11:04:03 +0000

    removing logs

--- venefica-site/application/logs/log-2013-04-29.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-04-29 14:33:54 --> Severity: Notice  --> Undefined variable: followings /home/gyuszi/EclipseWorkspace/venefica-site/application/views/pages/profile.php 51
-ERROR - 2013-04-29 14:33:54 --> Severity: Warning  --> Invalid argument supplied for foreach() /home/gyuszi/EclipseWorkspace/venefica-site/application/views/pages/profile.php 51
