884f2082143525c257ce902703ca511f2be6e4bc
avinash.yatko <avinash@venefica.labs>
2013-05-09 18:05:53 +0000

    added fields for followers, followings and followers data

--- venefica-android/Venefica/src/com/venefica/module/user/UserRegistrationResultWrapper.java
+++ venefica-android/Venefica/src/com/venefica/module/user/UserRegistrationResultWrapper.java
@@ -3,12 +3,20 @@
  */
 package com.venefica.module.user;
 
+import java.util.List;
+
+import com.venefica.services.ReviewDto;
+
 /**
+ * Wrapper class for user registration result data
+ * 
  * @author avinash
- * Wrapper class for user registration result data 
  */
 public class UserRegistrationResultWrapper {
 	public int result = -1;
 	public UserDto userDto = null;
 	public String data = null;
+	public List<UserDto> followings;
+	public List<UserDto> followers;
+	public List<ReviewDto> reviews;
 }
