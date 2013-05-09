182b7d194a64c332313d5e8486a9df5cecc75b71
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-05-09 06:13:09 +0000

    request status

--- trunk/venefica-webapp/src/main/java/com/venefica/model/RequestStatus.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/model/RequestStatus.java
@@ -1,18 +1,18 @@
-///*
-// * To change this template, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.venefica.model;
-//
-///**
-// * The 'lifecycle' of an ad request.
-// * 
-// * @author gyuszi
-// */
-//public enum RequestStatus {
-//    
-//    PENDING, //REQUESTED - giver didn't make a decision
-//    EXPIRED, //REJECTED - if someone else selected
-//    ACCEPTED, //selected
-//    ;
-//}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.venefica.model;
+
+/**
+ * The 'lifecycle' of an ad request.
+ * 
+ * @author gyuszi
+ */
+public enum RequestStatus {
+    
+    PENDING, //REQUESTED - giver didn't make a decision
+    EXPIRED, //REJECTED - if someone else selected
+    ACCEPTED, //selected
+    ;
+}
