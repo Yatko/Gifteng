d9ccee0fcbf62b285ac320654e6eda7f397958ba
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-05-09 18:06:33 +0000

    added functionality to show followers, followings and followers

--- trunk/venefica-android/Venefica/src/com/venefica/module/user/ViewProfileDetailsFragment.java
+++ trunk/venefica-android/Venefica/src/com/venefica/module/user/ViewProfileDetailsFragment.java
@@ -1,10 +1,14 @@
 package com.venefica.module.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,65 +17,99 @@ import com.actionbarsherlock.app.SherlockFragment;
 import com.venefica.module.utils.BadgeView;
 import com.venefica.module.main.R;
 import com.venefica.module.utils.Utility;
+import com.venefica.services.ReviewDto;
 import com.venefica.utils.Constants;
 import com.venefica.utils.VeneficaApplication;
 
 /**
- * @author avinash
- * Fragment to display user profile details
+ * @author avinash Fragment to display user profile details
  */
 public class ViewProfileDetailsFragment extends SherlockFragment {
 
 	/**
-	 * @author avinash
-	 * listener to communicate with activity
+	 * @author avinash listener to communicate with activity
 	 */
 	public interface ViewProfileDetailsListener {
 		/**
 		 * Method to get user details
+		 * 
 		 * @return UserDto
 		 */
 		public UserDto getUserDetails();
+
 		public void onViewMessages();
 	}
+
 	/**
 	 * user info
 	 */
 	private UserDto userDto;
 	/**
-     * view to show user details
-     */
-    private TextView txtUserName, txtMemberInfo, txtAddress;
-    private ImageView profImgView; 
-    private ImageButton imgButtonMsg;
-    private BadgeView badgeView;
+	 * view to show user details
+	 */
+	private TextView txtUserName, txtMemberInfo, txtAddress;
+	private ImageView profImgView;
+	private ImageButton imgButtonMsg;
+	private BadgeView badgeView;
+
+	private ExpandableListView userExpandableListView;
+	private ProfileExpandableListAdapter userExpandableListAdapter;
+	private ArrayList<UserDto> userFollowingList;
+	private ArrayList<UserDto> userFollowerList;
+	
+	private ExpandableListView reviewExpandableListView;
+	private ArrayList<ReviewDto> userReviewList;
+	private ReviewsExpandableListAdapter reviewsExpandableListAdapter;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		userFollowingList = new ArrayList<UserDto>();
+		userFollowerList = new ArrayList<UserDto>();
+		userReviewList = new ArrayList<ReviewDto>();
+		userExpandableListAdapter = new ProfileExpandableListAdapter(getActivity(), userFollowingList
+				, userFollowerList, userReviewList);
+		
+		reviewsExpandableListAdapter = new ReviewsExpandableListAdapter(getActivity(), userReviewList);
+	}
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.view_show_profile_details, container, false);
+		View view = inflater.inflate(R.layout.view_show_profile_details,
+				container, false);
 		view.findViewById(R.id.imgBtnUserViewFollow).setVisibility(View.GONE);
+
+		//Reviews
+		reviewExpandableListView = (ExpandableListView) view.findViewById(R.id.expListViewActUserProfieReviews);
+		reviewExpandableListView.setAdapter(reviewsExpandableListAdapter);
+		//expandable list view follower followings
+		userExpandableListView = (ExpandableListView) view.findViewById(R.id.expListViewActUserProfie);
+		userExpandableListView.setAdapter(userExpandableListAdapter);		
 		
-		 //user details
-        txtUserName = (TextView) view.findViewById(R.id.txtUserViewUserName);
-        txtMemberInfo = (TextView) view.findViewById(R.id.txtUserViewMemberInfo);
-        txtAddress = (TextView) view.findViewById(R.id.txtUserViewAddress);
-        profImgView = (ImageView) view.findViewById(R.id.imgUserViewProfileImg);
-        setUserDto(((VeneficaApplication) getActivity().getApplication()).getUser());
-        imgButtonMsg = (ImageButton) view.findViewById(R.id.imgBtnUserViewSendMsg);
-        badgeView = new BadgeView(getActivity(), imgButtonMsg);
-        badgeView.setText("2");
-        badgeView.setTextColor(Color.BLUE);
-        badgeView.setBadgeBackgroundColor(Color.YELLOW);
-//        badgeView.setTextSize(12);
-        badgeView.toggle();
+		// user details
+		txtUserName = (TextView) view.findViewById(R.id.txtUserViewUserName);
+		txtMemberInfo = (TextView) view
+				.findViewById(R.id.txtUserViewMemberInfo);
+		txtAddress = (TextView) view.findViewById(R.id.txtUserViewAddress);
+		profImgView = (ImageView) view.findViewById(R.id.imgUserViewProfileImg);
+		setUserDto(((VeneficaApplication) getActivity().getApplication())
+				.getUser());
+		imgButtonMsg = (ImageButton) view
+				.findViewById(R.id.imgBtnUserViewSendMsg);
+		badgeView = new BadgeView(getActivity(), imgButtonMsg);
+		badgeView.setText("2");
+		badgeView.setTextColor(Color.BLUE);
+		badgeView.setBadgeBackgroundColor(Color.YELLOW);
+		// badgeView.setTextSize(12);
+		badgeView.toggle();
 		return view;
 	}
-	
+
 	/**
 	 * Set user info
+	 * 
 	 * @param userDto
 	 */
-	public void setUserDto(UserDto userDto){
+	public void setUserDto(UserDto userDto) {
 		this.userDto = userDto;
 		if (userDto != null) {
 			txtUserName.setText(userDto.getFirstName() + " "
@@ -95,5 +133,34 @@ public class ViewProfileDetailsFragment extends SherlockFragment {
 										R.drawable.icon_picture_white));
 			}
 		}
+	}
+
+	/**
+	 * Method to update followings list
+	 * @param followingsList
+	 */
+	public void setFollowings(List<UserDto> followingsList) {
+		userFollowingList.clear();
+		userFollowingList.addAll(followingsList);
+		userExpandableListAdapter.notifyDataSetChanged();
+	}
+
+	/**
+	 * Method to update followers
+	 * @param followersList
+	 */
+	public void setFollowers(List<UserDto> followersList) {
+		userFollowerList.clear();
+		userFollowerList.addAll(followersList);
+		userExpandableListAdapter.notifyDataSetChanged();
+	}
+	/**
+	 * Method to update reviews
+	 * @param reviewsList
+	 */
+	public void setReviews(List<ReviewDto> reviewsList) {
+		reviewsList.clear();
+		reviewsList.addAll(reviewsList);
+		reviewsExpandableListAdapter.notifyDataSetChanged();
 	}
 }
