11e9ac6eff8c9d5d382d14512ab1b3bc913f2fda
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-05-09 20:27:31 +0000

    comment service refactored into message service

--- trunk/venefica-site/application/libraries/message_service.php
+++ trunk/venefica-site/application/libraries/message_service.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Message service
+ * Comment and message service
  *
  * @author gyuszi
  */
@@ -11,4 +11,27 @@ class Message_service {
         log_message(DEBUG, "Initializing Message_service");
     }
     
+    //**************
+    //* commenting *
+    //**************
+    
+    public function getCommentsByAd($adId, $lastCommentId, $numComments) {
+        try {
+            $commentService = new SoapClient(MESSAGE_SERVICE_WSDL, getSoapOptions(loadToken()));
+            $result = $commentService->getCommentsByAd(array(
+                "adId" => $adId,
+                "lastCommentId" => $lastCommentId,
+                "numComments" => $numComments
+            ));
+            
+            $comments = array();
+            if ( hasField($result, 'comment') && $result->comment ) {
+                $comments = Comment_model::convertComments($result->comment);
+            }
+            return $comments;
+        } catch ( Exception $ex ) {
+            log_message(ERROR, 'Comments request failed! '.$ex->faultstring);
+            throw new Exception($ex->faultstring);
+        }
+    }
 }
