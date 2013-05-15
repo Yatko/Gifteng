da5da23e304e31aa4bcfbfef9448e5dfeea487da
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-05-15 08:44:07 +0000

    using domain class name in HQL

--- trunk/venefica-webapp/src/main/java/com/venefica/dao/RequestDaoImpl.java
+++ trunk/venefica-webapp/src/main/java/com/venefica/dao/RequestDaoImpl.java
@@ -33,7 +33,7 @@ public class RequestDaoImpl extends DaoBase<Request> implements RequestDao {
     @Override
     public Request get(Long userId, Long adId) {
         // @formatter:off
-        return (Request) createQuery("from Request r where r.deleted = false and r.user.id = :userId and r.ad.id = :adId")
+        return (Request) createQuery("from " + getDomainClassName() + " r where r.deleted = false and r.user.id = :userId and r.ad.id = :adId")
                 .setParameter("userId", userId)
                 .setParameter("adId", adId)
                 .uniqueResult();
@@ -43,7 +43,7 @@ public class RequestDaoImpl extends DaoBase<Request> implements RequestDao {
     @Override
     public List<Request> getByAd(Long adId) {
         // @formatter:off
-        return createQuery("from Request r where r.deleted = false and r.ad.id = :adId")
+        return createQuery("from " + getDomainClassName() + " r where r.deleted = false and r.ad.id = :adId")
                 .setParameter("adId", adId)
                 .list();
         // @formatter:off
@@ -52,7 +52,7 @@ public class RequestDaoImpl extends DaoBase<Request> implements RequestDao {
     @Override
     public List<Request> getByUser(Long userId) {
         // @formatter:off
-        return createQuery("from Request r where r.deleted = false and r.user.id = :userId")
+        return createQuery("from " + getDomainClassName() + " r where r.deleted = false and r.user.id = :userId")
                 .setParameter("userId", userId)
                 .list();
         // @formatter:off
@@ -61,7 +61,7 @@ public class RequestDaoImpl extends DaoBase<Request> implements RequestDao {
     @Override
     public List<Request> getForUser(Long userId) {
         // @formatter:off
-        return createQuery("from Request r where r.deleted = false and r.ad.creator.id = :userId")
+        return createQuery("from " + getDomainClassName() + " r where r.deleted = false and r.ad.creator.id = :userId")
                 .setParameter("userId", userId)
                 .list();
         // @formatter:off
