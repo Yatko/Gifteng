9aa4b5246b9e55bf1145afa2b972698e0e9adbe0
avinash.yatko <avinash.yatko@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-05-21 19:15:22 +0000

    added share buttons functionality

--- trunk/venefica-android/Venefica/src/com/venefica/module/main/StartUpActivity.java
+++ trunk/venefica-android/Venefica/src/com/venefica/module/main/StartUpActivity.java
@@ -1,15 +1,22 @@
 package com.venefica.module.main;
 
 
+import java.util.List;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
@@ -42,6 +49,7 @@ public class StartUpActivity extends VeneficaActivity implements OnClickListener
 	 * invitation fragment
 	 */
 	private RequestInvitationFragment invitationFragment;
+	private ImageButton btnShareTweeter, btnShareFacebook, btnShareGmail, btnShareSms;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -58,6 +66,15 @@ public class StartUpActivity extends VeneficaActivity implements OnClickListener
 		invitationFragment = new RequestInvitationFragment();
 		fragmentTransaction.add(R.id.layActStartupRoot, invitationFragment);
 		fragmentTransaction.commit();
+		
+		btnShareTweeter = (ImageButton) findViewById(R.id.imgBtnActLoginShareTweeter);
+		btnShareTweeter.setOnClickListener(this);
+		btnShareFacebook = (ImageButton) findViewById(R.id.imgBtnActLoginShareFacebook);
+		btnShareFacebook.setOnClickListener(this);
+		btnShareGmail = (ImageButton) findViewById(R.id.imgBtnActLoginShareMail);
+		btnShareGmail.setOnClickListener(this);
+		btnShareSms = (ImageButton) findViewById(R.id.imgBtnActLoginShareChat);
+		btnShareSms.setOnClickListener(this);
 	}
 
 		
@@ -130,11 +147,109 @@ public class StartUpActivity extends VeneficaActivity implements OnClickListener
 	public void onClick(View view) {
 		int id = view.getId();
 		if (!uploadingData) {
-			
+			switch (id) {
+			case R.id.imgBtnActLoginShareTweeter:
+				shareOnTweeter();
+				break;
+			case R.id.imgBtnActLoginShareFacebook:
+				shareOnFacebook();
+				break;
+			case R.id.imgBtnActLoginShareMail:
+				shareWithGmail();
+				break;
+			case R.id.imgBtnActLoginShareChat:
+				shareWithSMS();
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
 		
+	/**
+	 * Mehod to send sms
+	 */
+	private void shareWithSMS() {
+		String shareText = getResources().getString(R.string.app_name)
+				+" "+ getResources().getString(R.string.g_geftang_url);
+		Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+		sendIntent.putExtra("sms_body", shareText); 
+		sendIntent.setType("vnd.android-dir/mms-sms");
+		startActivity(sendIntent);
+	}
+
+
+	/**
+	 * Method to share on facebook
+	 */
+	private void shareOnFacebook() {
+		Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+		shareIntent.setType("text/plain");
+		String shareText = getResources().getString(R.string.app_name)+" "
+				+ getResources().getString(R.string.g_label_share)+" "+ getResources().getString(R.string.g_facebook_share);
+		shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareText);
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+		for (final ResolveInfo app : activityList) {
+		    if ((app.activityInfo.name).contains("facebook")) {
+		        final ActivityInfo activity = app.activityInfo;
+		        final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
+		        shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		        shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+		        shareIntent.setComponent(name);
+		        startActivity(shareIntent);
+		        break;
+		   }
+		}
+	}
+
+
+	/**
+	 * Method to share on tweeter
+	 */
+	private void shareOnTweeter() {
+		Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+		shareIntent.setType("text/plain");
+		String shareText = getResources().getString(R.string.app_name)+" "
+				+ getResources().getString(R.string.g_label_share)+" "+ getResources().getString(R.string.g_twitter_share);
+		shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareText);
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+		for (final ResolveInfo app : activityList) {
+		    if ("com.twitter.android.PostActivity".equals(app.activityInfo.name)) {
+		        final ActivityInfo activity = app.activityInfo;
+		        final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
+		        shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		        shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+		        shareIntent.setComponent(name);
+		        startActivity(shareIntent);
+		        break;
+		   }
+		}
+	}
+
+	/**
+	 * method to share with gmail
+	 */
+	private void shareWithGmail(){
+		String to = "";
+		String subject = getResources().getString(R.string.app_name);
+		String message = getResources().getString(R.string.app_name) + " "
+				+ getResources().getString(R.string.g_label_share) + " "
+				+ getResources().getString(R.string.g_geftang_url);
+
+		Intent email = new Intent(Intent.ACTION_SEND);
+		email.putExtra(Intent.EXTRA_EMAIL, new String[] { to });
+		email.putExtra(Intent.EXTRA_SUBJECT, subject);
+		email.putExtra(Intent.EXTRA_TEXT, message);
+
+		// need this to prompts email client only
+		email.setType("message/rfc822");
+
+		startActivity(Intent.createChooser(email, getResources().getString(R.string.g_label_choose_email)));
+	}
+
 	/**
 	 * Start home screen of application
 	 */
