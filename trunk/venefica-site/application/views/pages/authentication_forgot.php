54ba85ca2fece447bff83050d843152461e44189
venefica.yatko.com <venefica.yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-08-02 11:02:20 +0000

    Updated PHP files to use latest HTML pages /application/views/pages/

--- trunk/venefica-site/application/views/pages/authentication_forgot.php
+++ trunk/venefica-site/application/views/pages/authentication_forgot.php
@@ -13,100 +13,113 @@
         
         <? if ( isset($authentication_forgot_password_requested) && $authentication_forgot_password_requested ): ?>
         
-            <div id="login_4" class="span4 ge-form ge-topspace">
-                <div class="well ge-well">
-                    <div class="row-fluid ge-text">
-                        <div class="row-fluid ge-topspace">
-                            <div class="span10 offset1 text-center ge-logo">
-                                <img src="<?=BASE_PATH?>temp-sample/gifteng.png" alt="Gifteng" />
-                            </div>
-                        </div><!--./gifteng-->
-
-                        <div class="row-fluid">
-                            <div class="span10 offset1">
-                                <p>
-                                    Success!<br />
-                                    You should receive an email shortly.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div class="row-fluid text-center">
-                            <div class="span10 offset1">
-                                <i class="fui-facebook lead"></i>
-                                <i class="fui-twitter lead"></i>
-                                <i class="fui-pinterest lead"></i>
-                            </div>
-                        </div><!--./email-->
-
-                    </div>	
-                    <div class="row-fluid ge-footline">
-                        <div class="span12">
-                            <p style="text-align:left;"><a href="<?=base_url()?>authorization/login">Sign in here &raquo;</a></p>
-                        </div>
-                    </div>
-                </div><!--./ge well-->
-            </div><!--./login step4-->
+            <!-- #login4 -->
+			<div id="login4" class="span6"><!-- ge-topspace outside of span -->
+				<div class="ge-topspace">
+					<div class="well ge-well ge-authbox">
+						<div class="ge-well-content ge-form">
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="ge-logo">
+										<i class="ge-icon-gifteng ge-icon-gifteng-large"></i>
+									</div>
+								</div>
+							</div>
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<p>
+										Success!<br />
+										You should receive an email shortly.
+									</p>
+								</div>
+							</div>
+					
+							<div class="row-fluid text-center">
+								<div class="span8 offset2">
+									<i class="ge-icon-facebook lead"></i>
+									<i class="ge-icon-twitter lead"></i>
+									<i class="ge-icon-pinterest lead"></i>
+								</div>
+							</div><!--./social icons-->
+							
+						</div>
+						<div class="ge-footline">
+							<div class="row-fluid">
+								<div class="span12">
+					 				<p class="text-left"><a href="<?=base_url()?>authorization/login">Sign in here &raquo;</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div><!-- #/login4 -->
         
         <? else: ?>
             
             <?=form_open('/authentication/forgot')?>
             
-            <div id="login_3" class="span4 ge-form ge-topspace">
-                <div class="well ge-well">
-                    <div class="row-fluid ge-text">
-                        <div class="row-fluid ge-topspace">
-                            <div class="span10 offset1 text-center ge-logo">
-                                <img src="<?=BASE_PATH?>temp-sample/gifteng.png" alt="Gifteng" />
-                            </div>
-                        </div><!--./gifteng-->
-
-                        <div class="row-fluid">
-                            <div class="span10 offset1">
-                                <p>
-                                    Please enter your email address so we can look up your account:
-                                </p>
-                            </div>
-                        </div>
-                        
+            <!-- #login3 -->
+			<div id="login3" class="span6"><!-- ge-topspace outside of span -->
+				<div class="ge-topspace">
+					<div class="well ge-well ge-authbox">
+						<div class="ge-well-content ge-form">
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="ge-logo">
+										<i class="ge-icon-gifteng ge-icon-gifteng-large"></i>
+									</div>
+								</div>
+							</div>
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<p>
+										Please enter your email address so we can look up your account:
+									</p>
+								</div>
+							</div>
+							
                         <? if( isset($this->forgot_password_form) && $this->forgot_password_form->hasErrors() ): ?>
                             <div class="row-fluid">
-                                <div class="span10 offset1">
+                                <div class="span8 offset2">
                                     <p class="error">
                                         <?=$this->forgot_password_form->error_string()?>
                                     </p>
                                 </div>
                             </div>
                         <? endif; ?>
-                        
-                        <div class="row-fluid">
-                            <div class="span10 offset1">
-                                <div class="control-group large">
-                                    <div class="controls">
-                                        <input name="forgot_password_email" value="<?=set_value('forgot_password_email')?>" type="text" placeholder="<?=lang('forgot_password_email_hint')?>" class="span3" required="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div><!--./email-->
-
-                        <div class="row-fluid ge-submit">
-                            <div class="span10 offset1">
-                                <div class="control-group">
-                                    <div class="controls">
-                                        <button type="submit" class="btn btn-large btn-block btn-ge"><?=lang('forgot_password_button')?></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div><!--./submit-->
-
-                    </div>	
-                    <div class="row-fluid ge-footline">
-                        <div class="span12">
-                            <p style="text-align:left;"><a href="<?=base_url()?>invitation/request">New to Gifteng? Request an invitation now &raquo;</a></p>
-                        </div>
-                    </div>
-                </div><!--./ge well-->
-            </div><!--./login step3-->
+					
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="control-group large">
+										<div class="controls">
+											<input name="forgot_password_email" type="email" value="<?=set_value('forgot_password_email')?>" placeholder="<?=lang('forgot_password_email_hint')?>" class="span3">
+										</div>
+									</div>
+								</div>
+							</div><!--./email-->
+					
+					
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="control-group">
+									  <div class="controls">
+									    <button type="submit" class="btn btn-large btn-block btn-ge"><?=lang('forgot_password_button')?></button>
+									  </div>
+									</div>
+								</div>
+							</div><!--./submit-->
+							
+						</div>
+						<div class="ge-footline">
+							<div class="row-fluid">
+								<div class="span12">
+					 				<p class="text-left"><a href="<?=base_url()?>invitation/request">New to Gifteng? Request an invitation now &raquo;</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div><!-- #/login3 -->
             
             <?=form_close()?>
             
--- trunk/venefica-site/application/views/pages/authentication_login.php
+++ trunk/venefica-site/application/views/pages/authentication_login.php
@@ -13,74 +13,88 @@
         
         <?=form_open('/authentication/login')?>
         
-        <div id="login_2" class="span4 ge-form ge-topspace">
-            <div class="well ge-well">
-                <div class="row-fluid ge-text">
-                    <div class="row-fluid ge-topspace">
-                        <div class="span10 offset1 text-center ge-logo">
-                            <img src="<?=BASE_PATH?>temp-sample/gifteng.png" alt="Gifteng" />
-                        </div>
-                    </div><!--./gifteng-->
-
-                    <? if( isset($this->login_form) && $this->login_form->hasErrors() ): ?>
-                        <div class="row-fluid">
-                            <div class="span10 offset1">
-                                <p class="error">
-                                    <?=$this->login_form->error_string()?>
-                                </p>
-                            </div>
-                        </div>
-                    <? endif; ?>
-                    
-                    <div class="row-fluid">
-                        <div class="span10 offset1">
-                            <div class="control-group large">
-                                <div class="controls">
-                                    <input name="login_email" value="<?=set_value('login_email')?>" type="text" placeholder="<?=lang('login_email_hint')?>" class="span3" required="">
-                                </div>
-                            </div>
-                        </div>
-                    </div><!--./email-->
-
-                    <div class="row-fluid">
-                        <div class="span10 offset1">
-                            <div class="control-group large">
-                                <div class="controls">
-                                    <input name="login_password" type="password" placeholder="<?=lang('login_password_hint')?>" class="span3" required="">
-                                </div>
-                            </div>
-                        </div>
-                    </div><!--./passowrd-->
-
-                    <div class="row-fluid ge-submit">
-                        <div class="span10 offset1">
-                            <div class="control-group">
-                                <div class="controls">
-                                    <button type="submit" class="btn btn-large btn-block btn-ge"><?=lang('login_login_button')?></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div><!--./submit-->
-
-                    <div class="row-fluid">
-                        <div class="span5 offset1">
-                            <label class="checkbox inline" for="remember_me">
-                                <input name="login_remember_me" value="1" <?=set_checkbox('login_remember_me', '1')?> id="remember_me" type="checkbox" data-toggle="checkbox">
-                                <?=lang('login_remember_me')?>
-                            </label>
-                        </div>
-                        <div class="span5">
-                            <a href="<?=base_url()?>authentication/forgot" class="optional pull-right">Forgot password</a>
-                        </div>
-                    </div>
-                </div>	
-                <div class="row-fluid ge-footline">
-                    <div class="span12">
-                        <p style="text-align:left;"><a href="<?=base_url()?>invitation/request">New to Gifteng? Request an invitation now &raquo;</a></p>
-                    </div>
-                </div>
-            </div><!--./ge well-->
-        </div><!--./login step2-->
+        <!-- #login2 -->
+			<div id="login2" class="span6"><!-- ge-topspace outside of span -->
+				<div class="ge-topspace">
+					<div class="well ge-well ge-authbox">
+						<div class="ge-well-content ge-form">
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="ge-logo">
+										<i class="ge-icon-gifteng ge-icon-gifteng-large"></i>
+									</div>
+								</div>
+							</div>
+							<? if( isset($this->login_form) && $this->login_form->hasErrors() ): ?>
+	                        <div class="row-fluid">
+	                            <div class="span8 offset2">
+	                                <p class="error">
+	                                    <?=$this->login_form->error_string()?>
+	                                </p>
+	                            </div>
+	                        </div>
+		                    <? else : ?>
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<p>
+										&nbsp;
+									</p>
+								</div>
+							</div>
+		                    <? endif; ?>
+					
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="control-group large">
+										<div class="controls">
+											<input name="login_email" type="email" value="<?=set_value('login_email')?>" placeholder="<?=lang('login_email_hint')?>" class="span3" required>
+										</div>
+									</div>
+								</div>
+							</div><!--./email-->
+					
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="control-group large">
+										<div class="controls">
+											<input name="login_password" type="password" placeholder="<?=lang('login_password_hint')?>" class="span3" required>
+										</div>
+									</div>
+								</div>
+							</div><!--./password-->
+					
+							<div class="row-fluid">
+								<div class="span8 offset2">
+									<div class="control-group">
+									  <div class="controls">
+									    <button type="submit" class="btn btn-large btn-block btn-ge"><?=lang('login_login_button')?></button>
+									  </div>
+									</div>
+								</div>
+							</div><!--./submit-->
+							
+							<div class="row-fluid">
+								<div class="span4 offset2 mobile-two">
+									<label class="checkbox inline" for="checkbox_remember">
+									  <input name="login_remember_me" type="checkbox" value="1" <?=set_checkbox('login_remember_me', '1')?> id="checkbox_remember" data-toggle="checkbox">
+									  <?=lang('login_remember_me')?>
+									</label>
+								</div>
+								<div class="span4 mobile-two">
+									 <a href="<?=base_url()?>authentication/forgot" class="optional pull-right">Forgot password</a>
+								</div>
+							</div>
+						</div>
+						<div class="ge-footline">
+							<div class="row-fluid">
+								<div class="span12">
+					 				<p class="text-left"><a href="<?=base_url()?>invitation/request">New to Gifteng? Request an invitation now &raquo;</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div><!-- #/login2 -->
         
         <?=form_close()?>
         
