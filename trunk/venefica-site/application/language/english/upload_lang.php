523556f6056ce85d07347865d4d9ee0d07218c4e
yatko.com <yatko.com@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-08-07 10:09:15 +0000

    fixing typo

--- trunk/venefica-site/application/language/english/upload_lang.php
+++ trunk/venefica-site/application/language/english/upload_lang.php
@@ -1,4 +1,4 @@
 <?php
 
 $lang[''] = '';
-$lang['upload_invalid_dimensions'] = 'Picture width and height must be at least 500px. Please select a larger image or limit the photo size to 5GB.';
+$lang['upload_invalid_dimensions'] = 'Picture width and height must be at least 500px. Please select a larger image or limit the photo size to 5MB.';
