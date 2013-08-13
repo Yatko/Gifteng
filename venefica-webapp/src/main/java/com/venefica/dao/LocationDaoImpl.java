f86ecc2ebce37f6c1a3398db3ae4814cecbbc71c
Yatko <yatko@users.noreply.github.com>
2013-08-13 11:56:02 +0000

    changed zipcode lookup function: if the zipcode entered by the client was not found will retry without the 0 in front Issue #GIF-15 - Map - Zip Code file

--- venefica-webapp/src/main/java/com/venefica/dao/LocationDaoImpl.java
+++ venefica-webapp/src/main/java/com/venefica/dao/LocationDaoImpl.java
@@ -6,6 +6,7 @@ package com.venefica.dao;
 
 import com.venefica.model.Location;
 import java.util.List;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,14 +25,25 @@ public class LocationDaoImpl extends DaoBase<Location> implements LocationDao {
             throw new NullPointerException("zipcode");
         }
         
-        List<Location> locations = createQuery(""
+        String hql = ""
                 + "select l "
                 + "from " + getDomainClassName() + " l "
                 + "where l.zipcode like '%' || :zipcode || '%' "
-                + "")
+                + "";
+        List<Location> locations = createQuery(hql)
                 .setParameter("zipcode", zipcode)
                 .setMaxResults(1)
                 .list();
+        
+        if ( locations == null || locations.isEmpty() ) {
+            //trying to find without 0 in front
+            zipcode = StringUtils.stripStart(zipcode, "0");
+            locations = createQuery(hql)
+                    .setParameter("zipcode", zipcode)
+                    .setMaxResults(1)
+                    .list();
+        }
+        
         return locations.isEmpty() ? null : locations.get(0);
     }
     
