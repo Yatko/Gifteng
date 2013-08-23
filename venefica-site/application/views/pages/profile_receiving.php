68f97690fd4da0c9b4c33942ca8501b07429aaff
kalman.venczel <kalman.venczel>
2013-08-23 16:59:01 +0000

    - default result for empty pages fixed

--- venefica-site/application/views/pages/profile_receiving.php
+++ venefica-site/application/views/pages/profile_receiving.php
@@ -52,7 +52,7 @@
 	                    <div class="span12">
 		                    <div class="well ge-well">
 			                    <div class="row-fluid">
-				                    <div class="span10 offset2 text-center">
+				                    <div class="span10 offset1 text-center">
 				                    	<img src="<?=BASE_PATH?>images/ge-nothing_here.jpg" width="800" height="500" alt="Nothing here ... :(" />
 				                    </div>
 		                    </div>
