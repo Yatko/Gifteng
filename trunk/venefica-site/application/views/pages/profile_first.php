0f45eaa6431aa7c20b6cac8f17e22c3a7e66426b
kalman.venczel <kalman.venczel@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-08-26 16:57:49 +0000

    - fixed profile_first (offset) and image (GIF-127)

--- trunk/venefica-site/application/views/pages/profile_first.php
+++ trunk/venefica-site/application/views/pages/profile_first.php
@@ -2,7 +2,7 @@
     <div class="span12">
         <div class="well ge-well">
             <div class="row-fluid">
-                <div class="span10 offset2 text-center">
+                <div class="span10 offset1 text-center">
                     <img src="<?=BASE_PATH?>temp-sample/ge-post_your_first_gift.jpg" width="800" height="500" alt="Post Your First Gift Now!" />
                 </div>
             </div>
