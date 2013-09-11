604b6c61ce14eed32b4f4993eee0c63b0b830023
krasimir.stavrev <krasimir.stavrev@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-09-11 20:09:07 +0000

    GIF-171 and GIF-187

--- trunk/venefica-site/application/views/pages/invitation_facebook.php
+++ trunk/venefica-site/application/views/pages/invitation_facebook.php
@@ -1,24 +1,5 @@
 <script>
 	$(function() {
-		$.ajaxSetup({ cache: true });
-		$.getScript('//connect.facebook.net/en_UK/all.js', function(){
-			FB.init({
-				appId      : '285994388213208',
-				status     : true,
-				cookie     : true,
-				oauth	   : true,
-				xfbml      : true
-			});
-			FB.Event.subscribe('auth.authResponseChange', function(response) {
-				if (response.status === 'connected') {
-					showFriends();
-				} else if (response.status === 'not_authorized') {
-					FB.login();
-				} else {
-					FB.login();
-				}
-			});
-		});
 		
 		$('#filter_friends').keydown(function() {
 			var filter = $(this).val();
@@ -35,15 +16,14 @@
 		
 	});
 	function showFriends() {
-		$('#fblogin').remove();
 		FB.api('/me/friends', function(response) {
 		  $.each(response.data,function(index, value) {
 		  	var friendbox = '<div class="span3"><div class="well ge-well">';
 		  	friendbox += '<div class="ge-user">';
-		  	friendbox += '<div class="ge-user-image"><img src="https://graph.facebook.com/'+value.id+'/picture"></div>';
+		  	friendbox += '<div class="ge-user-image"><img src="https://graph.facebook.com/'+value.id+'/picture" class="img-rounded"></div>';
 		  	friendbox += '<div class="ge-detail">';
 		  	friendbox += '<div class="ge-name"><a>'+value.name+'</a></div>';
-		  	friendbox += '<div class="ge-points"><span id="btn_'+value.id+'" onclick="sendInvitation('+value.id+')" class="label link ge-user-unfollow">Invite</span></div>'
+		  	friendbox += '<div class="ge-points"><span id="btn_'+value.id+'" onclick="sendInvitation('+value.id+')" class="label link ge-user-unfollow">INVITE</span></div>'
 		  	friendbox += '</div></div></div></div>';
 		  	$('#friendlist').append(friendbox);
 		  });
@@ -63,18 +43,21 @@
 <div class="container">
     <div class="row">
     	<div class="span12">	
-    		<div class="text-center">
-				<fb:login-button size="large" id="fblogin">Login with Facebook to Invite Friends</fb:login-button>
-				<div class="row">
-					<div class="span6 offset3">
-						<div class="well ge-well">
-							<h4 style="padding-top:10px;">Invite friends</h4>
-							<input type="text" id="filter_friends" placeholder="Search friends" />
-						</div>
+			<div class="well ge-well friend-filter">
+				<div class="row-fluid">
+					<div class="span6 text-right">
+						<label>Select from the list or search by name</label>
+					</div>
+					<div class="span5 text-left">
+						<input type="text" id="filter_friends" placeholder="Search friends" />
 					</div>
 				</div>
 			</div>
-			<div class="row" id="friendlist"></div>
+			<div class="row">
+				<div id="friendlist">
+					
+				</div>
+			</div>
     	</div>
     </div>
 </div>
