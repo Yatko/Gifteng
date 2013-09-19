df11e6c1484778bd3b8cb4be31c2e3c69db48ab7
kalman.venczel <kalman.venczel@9d2a0dfc-e40e-43ea-a415-1512434f2307>
2013-09-19 09:07:43 +0000

    - top giftengers UI (HTML)

--- trunk/venefica-site/application/views/pages/stat.php
+++ trunk/venefica-site/application/views/pages/stat.php
@@ -8,20 +8,21 @@
 
 ?>
 
-<div class="row">
-    <div class="span12">
-        <div class="text-center">
-            Top Giftengers
-        </div>
-    </div>
-</div>
+	<div class="row">
+	    <div class="span12">
+	        <div class="text-center ge-bottomspace">
+	            Top Giftengers
+	        </div>
+	    </div>
+	</div>
 
 <? if ( isset($users) && is_array($users) && count($users) > 0 ): ?>
     
     <div class="row">
+    	<div class="ge-profile ge-detail-view">
     <? for ( $iii = 4; $iii > 0; $iii-- ): ?>
-    <div class="span3">
-    
+    		<div class="span4">
+    			<div class="well ge-well">
     <? foreach ($users as $index => $user): ?>
         <?
         if ( ($index + $iii) % 4 != 0 ) {
@@ -35,30 +36,34 @@
         $location = $user->getLocation();
         $points = $user->getPoints(false);
         ?>
-        
-        <div class="ge-user-image">
-            <a href="<?= $profile_link ?>"><img src="<?= $img ?>" class="img img-rounded"></a>
-        </div>
-        <div class="ge-detail">
-            <div class="ge-name">
-                <a href="<?= $profile_link ?>"><?= $name ?></a>
-            </div>
-            <? if ($joined != ''): ?>
-                <div class="ge-age">Giftenger since <?= $joined ?></div>
-            <? endif; ?>
-            <? if ($location != ''): ?>
-                <div class="ge-location"><?= $location ?></div>
-            <? endif; ?>
-            <? if ($points != ''): ?>
-                <div class="ge-points">
-                    Generosity Score <?= $points ?>
-                </div>
-            <? endif; ?>
-        </div>
+					<div class="row-fluid">
+						<div class="ge-user">
+							<div class="span12">
+								<div class="ge-user-image">
+									<a href="<?= $profile_link ?>"><img src="<?= $img ?>" alt="" class="img img-rounded"></a>
+								</div>
+								<div class="ge-detail">
+									<div class="ge-name"><a href="<?= $profile_link ?>"><?= $name ?></a></div>
+								<? if ($joined != ''): ?>
+							        <div class="ge-age">Giftenger since <?= $joined ?></div>
+							    <? endif; ?>
+							    <? if ($location != ''): ?>
+							        <div class="ge-location"><?= $location ?></div>
+							    <? endif; ?>
+							    <? if ($points != ''): ?>
+							        <div class="ge-points">
+							        	<span class="label">Generosity Score <?= $points ?></span>
+							        	<span class="label label-ge inactive">Follow</span>					            
+							        </div>
+							    <? endif; ?>
+							</div>
+						</div>
+					</div>
     <? endforeach; ?>
-    
-    </div>
+    			</div>
+    		</div>
     <? endfor; ?>
+    	</div>
     </div>
 
 <? endif; ?>
